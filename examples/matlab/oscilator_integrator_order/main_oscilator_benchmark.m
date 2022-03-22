0810468963c99734fe73179096b1711b9c65c819
armin.nurkanovic <armin.nurkanovic@imtek.uni-freiburg.de>
2022-03-22 16:18:00 +0100

    clean up of oscilator example

--- examples/matlab/oscilator_integrator_order/main_oscilator_benchmark.m
+++ examples/matlab/oscilator_integrator_order/main_oscilator_benchmark.m
@@ -4,30 +4,30 @@ clc
 close all
 
 import casadi.*
-
-% digits(64)
+%%
 scenario_name = 'irk_fesd';
 save_results = 0;
 use_fesd = 0;
-plot_solution_trajectory = 0;
 %% Benchmark settings
 % discretization settings
 N_stages  = 3;
 N_finite_elements = 1;
-
 d_vec = [1 2 3 4 5];
-M_vec  = round(logspace(1.5,3.4,10))+8;
-% % short
-M_vec  = round(logspace(1.4,2.5,10))+7;
-% d_vec = [4];
-% M_vec = [250];
+d_vec = [2 3];
 
-
-%% Experiment Set Up 
+%% Experiment Set Up
 % preproces of step-size  %to avoid exact switch detection by hitting the switch exactly with the given grid
 T = 2;
 ts = 1; % eact switching time
-N_sim_vec = round(logspace(log10(11),log10(2001),10));
+N_start = 11;
+N_end = 2001;
+N_samples = 10;
+
+N_start = 11;
+N_end = 350;
+N_samples = 3;
+
+N_sim_vec = round(logspace(log10(N_start),log10(N_end),N_samples));
 N_sim_vec = round(N_sim_vec);
 % make all number odd
 N_sim_vec(mod(N_sim_vec,2)==0) = N_sim_vec(mod(N_sim_vec,2)==0)+1;
@@ -42,10 +42,6 @@ if any(abs(Ns - round(Ns)) == 0)
     error('exact switch detection just by chance');
 end
 
-%%
-
-
-
 legend_str = {'Implicit Euler','IRK Radau 3','IRK Radau 5','IRK Radau 7','IRK Radau 9','IRK Radau 11','IRK Radau 13'};
 % legend_str = {'IRK GL-2','IRK GL-3','IRK GL-4','IRK GL-5','IRK GL-6','IRK GL-7','IRK GL-8'};
 legend_str = [legend_str(d_vec)];
@@ -54,21 +50,18 @@ legend_str = [legend_str(d_vec)];
 % collocation settings
 settings = default_settings_fesd();
 settings.collocation_scheme = 'radau';     % Collocation scheme: radau or legendre
-settings.mpcc_mode = 3;                    
+settings.mpcc_mode = 5;
 settings.s_elastic_max = 1e1;              % upper bound for elastic variables
 % Penalty/Relaxation paraemetr
 comp_tol = 1e-16;
 settings.comp_tol = comp_tol;
 settings.sigma_0 = 1e0;                     % starting smouothing parameter
 settings.sigma_N = 1e-15;                     % starting smouothing parameter
-settings.N_homotopy = 30 ;% number of steps
+settings.N_homotopy = 15 ;% number of steps
 settings.kappa = 0.05;                      % decrease rate
-
 %^ IPOPT Settings
 opts_ipopt.verbose = false;
 opts_ipopt.ipopt.max_iter = 800;
-opts_ipopt.ipopt.mu_strategy = 'adaptive';
-opts_ipopt.ipopt.mu_oracle = 'quality-function';
 opts_ipopt.ipopt.tol = comp_tol ;
 opts_ipopt.ipopt.print_level = 0;
 opts_ipopt.ipopt.honor_original_bounds = 'yes';
@@ -77,8 +70,7 @@ settings.opts_ipopt = opts_ipopt;
 % finite elements with switch detection
 settings.use_fesd = use_fesd;       % turn on moving finite elements algortihm
 settings.fesd_complementartiy_mode = 3;       % turn on moving finite elements algortihm
-settings.gamma_h = 1; 
-% how much can h addapt
+settings.gamma_h = 1;
 settings.equidistant_control_grid = 0;
 %% Time settings
 omega = 2*pi;
@@ -100,7 +92,6 @@ M_true_all_experiment  = [];
 
 %% Run experiment
 h_opt_full = [];
-
 for i = 1:length(d_vec)
     d = d_vec(i);
     n_col = N_stages*(d+1); % number of collocation points per 2 finite elements
@@ -111,50 +102,32 @@ for i = 1:length(d_vec)
     nominal_h_current_experiment = [];
     M_true_current_experiment  = [];
 
-    for  j = 1:length(M_vec)
-%         M = M_vec(j);
-%         N_sim = round(M/n_col); % total number of simulation intevals
-% 
-%         h = T_sim/(N_stages*N_finite_elements*N_sim);    % nominal step lenght of a single finite elemnt;
-%         M_true_current = N_sim*n_col;
-
-%         update step size
-%         model.T = N_stages*N_finite_elements*h;
-%         model.h = h;
-%         model.T_sim = T_sim;
-%  h_opt_full = [];
+    for  j = 1:length(N_sim_vec)
         h_outside = h_sim_vec(j); % integrator step of FESD (outside step length)
         N_sim = T_sim/h_outside;
         h_inside = h_outside/(N_finite_elements);    % nominal step lenght of a single finite elemnt;
         h = h_inside;
         M_true_current = N_sim*n_col;
         M = M_true_current;
-
         % update step size
         model.T = h_outside;
         model.T_sim = h_outside;
         model.h = h_inside/N_stages;
         model.N_sim = N_sim;
-
-
-        fprintf('Scheme with d = %d, current collocattion points %d , run: %d of %d \n',d,M_true_current,j,length(M_vec))
-
+        fprintf('Scheme with d = %d, current collocattion points %d , run: %d of %d \n',d,M_true_current,j,length(N_sim_vec))
         % generate new model with updated settings;
         model = oscilator(model);
-%         [model,settings] = model_reformulation_fesd(model,settings);
         [results,stats] = integrator_fesd(model,settings);
 
-         % numerical error
+        % numerical error
         x_fesd = results.x_res(:,end);
         error_x = norm(x_fesd-x_star,"inf");
-
-        % complementarity
         max_complementarity_exp = max(stats.complementarity_stats);
-        % save date current experiemnt
+
         errors_current_experiment = [errors_current_experiment,error_x];
         fprintf('Error with (h = %2.5f, M = %d, d = %d ) is %5.2e : \n',h,M,d,error_x);
         fprintf('Complementarity residual %5.2e : \n',max_complementarity_exp);
-
+        % save date current experiemnt
         complementarity_current_experiment = [complementarity_current_experiment,max_complementarity_exp];
         nominal_h_current_experiment = [nominal_h_current_experiment,h];
         M_true_current_experiment = [M_true_current_experiment,M_true_current];
@@ -166,18 +139,17 @@ for i = 1:length(d_vec)
 end
 
 %% Error plots
-% numerical error
 figure
-    for ii = 1:length(d_vec)
-        loglog(M_true_all_experiment(ii,:),errors_all_experiments(ii,:),'-o','linewidth',1.5);
-        hold on
-    end
+for ii = 1:length(d_vec)
+    loglog(M_true_all_experiment(ii,:),errors_all_experiments(ii,:),'-o','linewidth',1.5);
+    hold on
+end
 xlabel('$M$','interpreter','latex');
 ylabel('$E(2)$','interpreter','latex');
 grid on
 ylim([1e-14 100])
 legend(legend_str,'interpreter','latex');
-if save_results 
+if save_results
     saveas(gcf,[scenario_name '_error_M'])
 end
 % some stats
@@ -202,21 +174,21 @@ xlabel('$M$','interpreter','latex');
 ylabel('comp residual','interpreter','latex');
 grid on
 legend(legend_str,'interpreter','latex');
-if save_results 
+if save_results
     saveas(gcf,[scenario_name '_comp_residual_M'])
 end
 % error as fu
 figure
 for ii = 1:length(d_vec)
-loglog(nominal_h_all_experiments(ii,:),complementarity_all_experiments(ii,:)+1e-18,'-o','linewidth',1.5);
-hold on
+    loglog(nominal_h_all_experiments(ii,:),complementarity_all_experiments(ii,:)+1e-18,'-o','linewidth',1.5);
+    hold on
 end
 xlabel('$h$','interpreter','latex');
 ylabel('comp residual','interpreter','latex');
 grid on
 legend(legend_str,'interpreter','latex');
-if save_results 
-saveas(gcf,[scenario_name '_comp_residual_h'])
+if save_results
+    saveas(gcf,[scenario_name '_comp_residual_h'])
 end
 %% as function of step size
 figure
@@ -229,54 +201,16 @@ for ii = 1:length(d_vec)
 end
 ylim([1e-14 100])
 legend(legend_str,'interpreter','latex');
-if save_results 
-saveas(gcf,[scenario_name '_error_h'])
+if save_results
+    saveas(gcf,[scenario_name '_error_h'])
 end
-%% plot_solution_trajectory
-diff_states = results.x_res;
-h_opt_full = results.h_vec;
-if plot_solution_trajectory
-    x1_opt = diff_states(1,:);
-    x2_opt = diff_states(2,:);
-    tgrid = cumsum([0;h_opt_full]);
-    figure
-    subplot(121)
-        plot(tgrid,x1_opt,'linewidt',1.0);
-        grid on
-        hold on
-        plot(tgrid,x2_opt,'linewidt',1.0);
-        xline(1)
-        xlabel('$t$','interpreter','latex');
-        ylabel('$x(t)$','interpreter','latex');
-        legend({'$x_1(t)$','$x_2(t)$'},'interpreter','latex');
-%         xlim([1-1e-5 1+1e-5])
-%         ylim([0-1e-5 0+1e-5])
-    subplot(122)
-    plot(x1_opt,x2_opt,'linewidt',1.8);
-    hold on
-    fimplicit(@(x,y) (x-0).^2+(y-0).^2-1^2, [-3 3],'r','linewidth',1.5)
-    xlabel('$x_1$','interpreter','latex');
-    ylabel('$x_2$','interpreter','latex');
-    axis equal
-    grid on
-    [X,Y] = meshgrid(-3:0.35:3,-3:0.35:3);
-    [U,V] = oscilator_eval(X,Y);
-    quiver(X,Y,U,V,'Color',0.65*ones(3,1),'LineWidth',1.2,'AutoScaleFactor',3);
-    % [X,Y] = meshgrid(-1:0.1:1,-1:0.1:1);
-    % [U,V] = oscilator_eval(X,Y);
-    % quiver(X,Y,U,V,'Color',0.6*ones(3,1),'LineWidth',1.2,'AutoScaleFactor',2);
-    xlim([-exp(1) exp(1)]*1.01)
-    ylim([-exp(1) exp(1)]*0.9)
 
-end
 
 %% Practial slopes
 if length(M_vec) ==1
     fprintf('Error: %5.2e, h = %4.4f \n',errors_all_experiments,h)
 end
-%%
-figure
-stairs(results.h_vec)
+
 %%
 results.M_vec = M_vec;
 results.d_vec = d_vec;
@@ -285,6 +219,6 @@ results.errors_switch_detection_1_all_experiments =errors_switch_detection_1_all
 results.nominal_h_all_experiments =nominal_h_all_experiments;
 results.errors_all_experiments =errors_all_experiments;
 results.complementarity_all_experiments = complementarity_all_experiments;
-if save_results 
-save([scenario_name '.mat'],'results')
+if save_results
+    save([scenario_name '.mat'],'results')
 end
---
+++ examples/matlab/oscilator_integrator_order/main_oscilator_simulation.m
@@ -0,0 +1,116 @@
+clear all
+clc
+close all
+import casadi.*
+%%
+use_fesd = 1;
+plot_solution_trajectory = 1;
+%% Benchmark settings
+% discretization settings
+N_stages  = 3;
+N_finite_elements = 1;
+
+%% Experiment Set Up
+n_s = 9; % number of irk stages
+T = 2;
+ts = 1; % eact switching time
+N_sim  = 10;
+T_sim = T/N_sim;
+h = T_sim/(N_stages*N_finite_elements);
+%% settings
+% collocation settings
+settings = default_settings_fesd();
+settings.collocation_scheme = 'radau';     % Collocation scheme: radau or legendre
+settings.mpcc_mode = 5;
+settings.s_elastic_max = 1e1;              % upper bound for elastic variables
+% Penalty/Relaxation paraemetr
+comp_tol = 1e-16;
+settings.comp_tol = comp_tol;
+settings.sigma_0 = 1e0;                     % starting smouothing parameter
+settings.sigma_N = 1e-15;                     % starting smouothing parameter
+settings.N_homotopy = 15;% number of steps
+settings.kappa = 0.05;                      % decrease rate
+%^ IPOPT Settings
+opts_ipopt.verbose = false;
+opts_ipopt.ipopt.max_iter = 800;
+opts_ipopt.ipopt.mu_strategy = 'adaptive';
+opts_ipopt.ipopt.mu_oracle = 'quality-function';
+opts_ipopt.ipopt.tol = comp_tol ;
+opts_ipopt.ipopt.print_level = 0;
+opts_ipopt.ipopt.honor_original_bounds = 'yes';
+opts_ipopt.ipopt.bound_relax_factor = 1e-16;
+settings.opts_ipopt = opts_ipopt;
+% finite elements with switch detection
+settings.use_fesd = use_fesd;       % turn on moving finite elements algortihm
+settings.fesd_complementartiy_mode = 3;       % turn on moving finite elements algortihm
+settings.gamma_h = 1;
+% how much can h addapt
+settings.equidistant_control_grid = 0;
+%% Time settings
+omega = 2*pi;
+% analytic solution
+x_star = [exp(1);0];
+s_star = [exp(2)  0; exp(2)*2*omega exp(2)];
+t1_star = 1; % optimal siwtch points
+T = 2;                            % time budget of transformed pseudo time
+T_sim = T;
+model.N_stages = N_stages;
+model.N_finite_elements = N_finite_elements;
+
+
+
+settings.d = n_s;
+
+% update step size
+model.T = T_sim;
+model.T_sim = T_sim;
+model.h = h;
+model.N_sim = N_sim;
+model = oscilator(model);
+[results,stats] = integrator_fesd(model,settings);
+% numerical error
+x_fesd = results.x_res(:,end);
+error_x = norm(x_fesd-x_star,"inf");
+% complementarity
+max_complementarity_exp = max(stats.complementarity_stats);
+fprintf('Error with (h = %2.5f, n_s = %d ) is %5.2e : \n',h,n_s,error_x);
+fprintf('Complementarity residual %5.2e : \n',max_complementarity_exp);
+
+%% plot_solution_trajectory
+diff_states = results.x_res;
+h_opt_full = results.h_vec;
+x1_opt = diff_states(1,:);
+x2_opt = diff_states(2,:);
+tgrid = cumsum([0;h_opt_full]);
+figure
+subplot(121)
+plot(tgrid,x1_opt,'linewidt',1.0);
+grid on
+hold on
+plot(tgrid,x2_opt,'linewidt',1.0);
+xline(1)
+xlabel('$t$','interpreter','latex');
+ylabel('$x(t)$','interpreter','latex');
+legend({'$x_1(t)$','$x_2(t)$'},'interpreter','latex');
+%         xlim([1-1e-5 1+1e-5])
+%         ylim([0-1e-5 0+1e-5])
+subplot(122)
+plot(x1_opt,x2_opt,'linewidt',1.8);
+hold on
+fimplicit(@(x,y) (x-0).^2+(y-0).^2-1^2, [-3 3],'r','linewidth',1.5)
+xlabel('$x_1$','interpreter','latex');
+ylabel('$x_2$','interpreter','latex');
+axis equal
+grid on
+[X,Y] = meshgrid(-3:0.35:3,-3:0.35:3);
+[U,V] = oscilator_eval(X,Y);
+quiver(X,Y,U,V,'Color',0.65*ones(3,1),'LineWidth',1.2,'AutoScaleFactor',3);
+
+xlim([-exp(1) exp(1)]*1.01)
+ylim([-exp(1) exp(1)]*0.9)
+%%
+figure
+stairs(results.h_vec)
+xlabel('integration step')
+ylabel('$h$','Interpreter','latex');
+%%
