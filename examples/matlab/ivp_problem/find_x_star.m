ec34e8741f2884a43f945d0c4c47bd11d0386c83
armin.nurkanovic <armin.nurkanovic@imtek.uni-freiburg.de>
2022-04-14 11:31:44 +0200

    added ivp experiment from fesd paper

--- examples/matlab/ivp_problem/find_x_star.m
+++
@@ -1,7 +0,0 @@
-syms x real
-ts = -x/3;
-L =(8/3*ts.^3 +8/3*x.*ts.^2 + 8/9*x.^2.*ts +1/3*T^3+1/3*x.*T^2+x.^2*T/9) + (T+(x-5)/3).^2;
-nabla_L = diff(L,x)
-x_star = solve(nabla_L)
-a = double(x_star(1))
-b = double(x_star(2))
