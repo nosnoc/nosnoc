fa01d92c6f3ce1ca3baa19ef29badac6339cb1e8
armin.nurkanovic <armin.nurkanovic@imtek.uni-freiburg.de>
2022-10-02 20:28:50 +0200

    added robotic maniuplation example with three carts

---
+++ examples/matlab/three_cart_problem/three_cart_ocp.m
@@ -0,0 +1,245 @@
+%
+%    This file is part of NOSNOC.
+%
+%    NOSNOC -- A software for NOnSmooth Numerical Optimal Control.
+%    Copyright (C) 2022 Armin Nurkanovic, Moritz Diehl (ALU Freiburg).
+%
+%    NOSNOC is free software; you can redistribute it and/or
+%    modify it under the terms of the GNU Lesser General Public
+%    License as published by the Free Software Foundation; either
+%    version 3 of the License, or (at your option) any later version.
+%
+%    NOSNOC is distributed in the hope that it will be useful,
+%    but WITHOUT ANY WARRANTY; without even the implied warranty of
+%    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+%    Lesser General Public License for more details.
+%
+%    You should have received a copy of the GNU Lesser General Public
+%    License along with NOS-NOC; if not, write to the Free Software Foundation,
+%    Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+%
+%
+
+%% Three cart manipulation example
+% example taken from https://github.com/KY-Lin22/NIPOCPEC
+
+%% 
+clear all;
+close all;
+clc;
+import casadi.*
+
+% delete old gif
+delete three_carts.gif
+
+%%
+[settings] = default_settings_nosnoc();  
+settings.irk_scheme = 'Radau-IIA';
+settings.n_s = 1;  % number of stages in IRK methods
+
+settings.use_fesd = 1; 
+settings.mpcc_mode = 5; % \ell_inifnity penalization of the complementariy constraints
+settings.N_homotopy = 7;
+settings.cross_comp_mode = 3;
+settings.opts_ipopt.ipopt.max_iter = 1e3;
+settings.print_level = 3;
+settings.comp_tol = 1e-9;
+settings.time_freezing = 1;
+% settings.s_sot_max = 2;
+% settings.s_sot_min = 0.5;
+settings.equidistant_control_grid = 1; % equdistiant control grid in numerical time
+settings.pss_lift_step_functions = 1;
+settings.stagewise_clock_constraint = 1; % equdistiant control grid in physical time
+%% IF HLS solvers for Ipopt installed (check https://www.hsl.rl.ac.uk/catalogue/ and casadi.org for instructions) use the settings below for better perfmonace:
+% settings.opts_ipopt.ipopt.linear_solver = 'ma57';
+
+%% discretizatioon
+N_stg = 30; % control intervals
+N_FE = 2;  % integration steps per control intevral
+
+%% model parameters
+m1 = 1;
+m2 = 1;
+m3 = 1;
+cart_width1 = 2;
+cart_width2 = 2;
+cart_width3 = 2;
+k1 = 1*0;
+k2 = 1*0;
+k3 = 1*0;
+c_damping = 2;
+
+ubx = [10; 15; 15; 5; 5; 5]; 
+lbx = [-15; -15; -10; -5; -5; -5];            
+        
+x0 = [-3; 0; 3; 0; 0; 0];
+x_ref = [-5; 0; 5; 0; 0; 0];
+u_ref = [0; 0; 0];
+
+Q = diag([10; 1; 10; 0.1; 0.1; 0.1]);
+Q_terminal = 100*Q;
+R = diag([0.1; 0.1; 0.1]);
+
+u_max = 20;
+u_min = -20;
+
+lbu = [0*u_min; u_min; 0*u_min];
+ubu = [0*u_max; u_max; 0*u_max];
+
+
+%% Symbolic variables and bounds
+q = SX.sym('q',3); v = SX.sym('v',3); 
+u = SX.sym('u',3);
+x = [q;v];
+model.T = 4;
+model.N_stages = N_stg;
+model.N_finite_elements  = N_FE;
+model.x = x;
+model.u = u;
+model.e = 0;
+model.mu = 0.0;
+model.a_n = 10;
+model.x0 = x0; 
+
+% model.f = [1/m1*(u(1)-c_damping*v(1)-k1*q(1));...
+%            1/m2*(u(2)-c_damping*v(2)+k2*(q(1)-q(2)));...
+%            1/m3*(u(3)-c_damping*v(3)+k3*q(2))];
+% dot{v} r.h.s.
+
+model.M = diag([m1;m2;m3]); % inertia/mass matrix;
+model.f = [(u(1)-c_damping*v(1)-k1*q(1));...
+           (u(2)-c_damping*v(2)+k2*(q(1)-q(2)));...
+           (u(3)-c_damping*v(3)+k3*q(2))];
+
+% gap functions
+model.c = [q(2) - q(1) - 0.5*cart_width2 - 0.5*cart_width1;...
+           q(3) - q(2) - 0.5*cart_width3 - 0.5*cart_width2];
+
+
+% box constraints on controls and states
+model.lbu = lbu;
+model.ubu = ubu;
+model.lbx = lbx;
+model.ubx = ubx;
+% Stage cost
+model.f_q = (x-x_ref)'*Q*(x-x_ref)+ u'*R*u;
+% terminal cost
+model.f_q_T = (x-x_ref)'*Q_terminal*(x-x_ref);
+% model.g_terminal = [x-x_ref];
+%% Call nosnoc solver
+[results,stats,model,settings] = nosnoc_solver(model,settings);
+% [results] =
+polishing_homotopy_solution(model,settings,results,stats.sigma_k); % (experimental, projects and fixes active set at solution and solves NLP)
+%% read and plot results
+unfold_struct(results,'base');
+p1 = x_opt(1,:);
+p2 = x_opt(2,:);
+p3 = x_opt(3,:);
+v1 = x_opt(4,:);
+v2 = x_opt(5,:);
+v3 = x_opt(6,:);
+t_opt = x_opt(7,:);
+
+u1_opt = u_opt(1,:);
+u2_opt = u_opt(2,:);
+u3_opt = u_opt(3,:);
+
+
+%% animation
+figure('Renderer', 'painters', 'Position', [100 100 1400 600])
+
+carts_appart = 2;
+x_min =min([p1,p2,p3])-2.5;
+x_max = max([p1,p2,p3])+2.5;
+cart_height = 2;
+
+carts_appart = 1.5*1;
+for ii = 1:length(p1)
+    % cart 1
+    xp = [p1(ii)-cart_width1/2 p1(ii)+cart_height/2 p1(ii)+cart_height/2 p1(ii)-cart_width1/2];
+    yp = [0 0 cart_height  cart_height];
+    patch(xp,yp,'b','FaceAlpha',0.8)
+    hold on
+    % cart 2
+    xp = [p2(ii)-cart_width2/2 p2(ii)+cart_height/2 p2(ii)+cart_height/2 p2(ii)-cart_width2/2];
+    yp = [0 0 cart_height  cart_height];
+    patch(xp,yp,'r','FaceAlpha',0.8)
+
+    % cart 3
+    xp = [p3(ii)-cart_width3/2 p3(ii)+cart_height/2 p3(ii)+cart_height/2 p3(ii)-cart_width3/2];
+    yp = [0 0 cart_height  cart_height];
+    patch(xp,yp,'m','FaceAlpha',0.8)
+
+    if k2 >0
+    spring1 = linspace(min(p1(ii),p2(ii)),max(p1(ii),p2(ii)),5);
+    plot(spring1,spring1*0+cart_height/2,'ko-','LineWidth',1);
+    end
+
+    % the refereneces
+    % cart 1
+    xp = [x_ref(1)-cart_width1/2 x_ref(1)+cart_height/2 x_ref(1)+cart_height/2 x_ref(1)-cart_width1/2];
+    yp = [0 0 cart_height  cart_height];
+    patch(xp,yp,'b','FaceAlpha',0.05,'EdgeColor','none')
+    hold on
+    % cart 2
+    xp = [x_ref(2)-cart_width2/2 x_ref(2)+cart_height/2 x_ref(2)+cart_height/2 x_ref(2)-cart_width2/2];
+    yp = [0 0 cart_height  cart_height];
+    patch(xp,yp,'r','FaceAlpha',0.05,'EdgeColor','none')
+
+    % cart 3
+    xp = [x_ref(3)-cart_width3/2 x_ref(3)+cart_height/2 x_ref(3)+cart_height/2 x_ref(3)-cart_width3/2];
+    yp = [0 0 cart_height  cart_height];
+    patch(xp,yp,'m','FaceAlpha',0.05,'EdgeColor','none')
+
+
+    % ground     
+    xp = [x_min x_max x_max x_min ];
+    yp = [-1 -1 0 0];
+    patch(xp,yp,0*ones(1,3),'FaceAlpha',0.1,'EdgeColor','none');
+    
+    axis equal
+    xlim([x_min x_max])
+    ylim([-0.75 3.5])
+    pause(model.h_k);
+    try
+        exportgraphics(gcf,'three_carts.gif','Append',true);
+    catch
+        disp('the simple gif function is avilable for MATLAB2022a and newer.')
+    end
+    if ii~=length(p1)
+        clf;
+    end
+end
+
+%%
+figure('Renderer', 'painters', 'Position', [100 100 1400 600])
+subplot(131)
+plot(t_opt,p1,'LineWidth',1.5);
+hold on
+plot(t_opt,p2,'LineWidth',1.5);
+plot(t_opt,p3,'LineWidth',1.5);
+% axis equal
+grid on
+legend({'$p_1(t)$','$p_2(t)$','$p_3(t)$'},'interpreter','latex');
+xlabel('$t$','interpreter','latex');
+ylabel('$p$','interpreter','latex');
+% axis equal
+subplot(132)
+plot(t_opt,v1,'LineWidth',1.5);
+hold on
+plot(t_opt,v2,'LineWidth',1.5);
+plot(t_opt,v3,'LineWidth',1.5);
+legend({'$v_1(t)$','$v_2(t)$','$v_3(t)$'},'interpreter','latex');
+grid on
+xlabel('$t$','interpreter','latex');
+ylabel('$v$','interpreter','latex');
+
+subplot(133)
+stairs(t_opt(1:N_FE:end),[u1_opt,nan],'LineWidth',1.5);
+hold on
+stairs(t_opt(1:N_FE:end),[u2_opt,nan],'LineWidth',1.5);
+stairs(t_opt(1:N_FE:end),[u3_opt,nan],'LineWidth',1.5);
+legend({'$u_1(t)$','$u_2(t)$','$u_3(t)$'},'interpreter','latex');
+grid on
+xlabel('$t$','interpreter','latex');
+ylabel('$u$','interpreter','latex');
---
+++ examples/matlab/three_cart_problem/three_cart_sim.m
@@ -0,0 +1,153 @@
+%% Three crat problem - simulation
+
+clear all;
+close all;
+clc;
+import casadi.*
+
+%% model parameters
+m1 = 1;
+m2 = 1;
+m3 = 1;
+cart_width1 = 2;
+cart_width2 = 2;
+cart_width3 = 2;
+k1 = 1*0;
+k2 = 1*0;
+k3 = 1*0;
+c_damping = 2;
+u_max = 20;
+u_min = -20;
+ubx = [10; 15; 15; 5; 5; 5]; 
+lbx = [-15; -15; -10; -5; -5; -5];            
+          
+x0 = [-3; 0; 3; 0; 0; 0];
+
+%%
+[settings] = default_settings_nosnoc();  
+settings.irk_scheme = 'Radau-IIA';
+settings.n_s = 1;
+
+settings.mpcc_mode = 5;
+
+settings.opts_ipopt.ipopt.max_iter = 5e2;
+settings.print_level = 2;
+settings.N_homotopy = 12;
+settings.use_fesd = 1;
+settings.cross_comp_mode = 8;
+settings.time_freezing = 1;
+settings.pss_lift_step_functions = 1;
+settings.time_freezing_reduced_model = 0;
+
+settings.impose_terminal_phyisical_time = 1;
+settings.local_speed_of_time_variable = 1;
+settings.stagewise_clock_constraint = 0;
+
+% settings.rho_h = 0;
+settings.delta_h_regularization = 1;
+settings.step_equilibration = 0;
+
+settings.opts_ipopt.ipopt.linear_solver = 'ma57';
+
+%%
+% Symbolic variables and bounds
+q = SX.sym('q',3); v = SX.sym('v',3); 
+model.x = [q;v]; 
+model.e = 0;
+model.mu = 0;
+model.a_n = 100;
+model.x0 = x0; 
+% fixed control
+u = [-1;10;0];
+model.f = [1/m1*(u(1)-c_damping*v(1)-k1*q(1));...
+           1/m2*(u(2)-c_damping*v(2)+k2*(q(1)-q(2)));...
+           1/m3*(u(3)-c_damping*v(3)+k3*q(2))];
+
+model.c = [q(2) - q(1) - 0.5*cart_width2 - 0.5*cart_width1;...
+           q(3) - q(2) - 0.5*cart_width3 - 0.5*cart_width2];
+
+%% Simulation setings
+N_FE = 2;
+T_sim = 3;
+N_sim = 60;
+model.T_sim = T_sim;
+model.N_FE = N_FE;
+model.N_sim = N_sim;
+settings.use_previous_solution_as_initial_guess = 1;
+%% Call nosnoc Integrator
+[results,stats,model] = integrator_fesd(model,settings);
+%% read and plot results
+unfold_struct(results,'base');
+p1 = x_res(1,:);
+p2 = x_res(2,:);
+p3 = x_res(3,:);
+v1 = x_res(4,:);
+v2 = x_res(5,:);
+v3 = x_res(6,:);
+t_opt = x_res(7,:);
+%%
+figure
+subplot(121)
+plot(t_opt,p1,'LineWidth',1.5);
+hold on
+plot(t_opt,p2,'LineWidth',1.5);
+plot(t_opt,p3,'LineWidth',1.5);
+% axis equal
+grid on
+legend({'$p_1(t)$','$p_2(t)$','$p_3(t)$'},'interpreter','latex');
+xlabel('$t$','interpreter','latex');
+ylabel('$p$','interpreter','latex');
+% axis equal
+subplot(122)
+plot(t_opt,v1,'LineWidth',1.5);
+hold on
+plot(t_opt,v2,'LineWidth',1.5);
+plot(t_opt,v3,'LineWidth',1.5);
+legend({'$v_1(t)$','$v_2(t)$','$v_3(t)$'},'interpreter','latex');
+grid on
+xlabel('$t$','interpreter','latex');
+ylabel('$v$','interpreter','latex');
+
+%% animation
+figure('Renderer', 'painters', 'Position', [100 100 1400 600])
+
+carts_appart = 2;
+x_min =min([p1,p2,p3])-2.5;
+x_max = max([p1,p2,p3])+2.5;
+cart_height = 2;
+
+carts_appart = 1.5*1;
+for ii = 1:length(p1)
+    % cart 1
+    xp = [p1(ii)-cart_width1/2 p1(ii)+cart_height/2 p1(ii)+cart_height/2 p1(ii)-cart_width1/2];
+    yp = [0 0 cart_height  cart_height];
+    patch(xp,yp,'b','FaceAlpha',0.4)
+    hold on
+    % cart 2
+    xp = [p2(ii)-cart_width2/2 p2(ii)+cart_height/2 p2(ii)+cart_height/2 p2(ii)-cart_width2/2];
+    yp = [0 0 cart_height  cart_height];
+    patch(xp,yp,'r','FaceAlpha',0.4)
+
+    if k2 >0
+    spring1 = linspace(min(p1(ii),p2(ii)),max(p1(ii),p2(ii)),5);
+    plot(spring1,spring1*0+cart_height/2,'ko-','LineWidth',1);
+    end
+
+    % cart 3
+    xp = [p3(ii)-cart_width3/2 p3(ii)+cart_height/2 p3(ii)+cart_height/2 p3(ii)-cart_width3/2];
+    yp = [0 0 cart_height  cart_height];
+    patch(xp,yp,'m','FaceAlpha',0.4)
+
+    % ground     
+    xp = [x_min x_max x_max x_min ];
+    yp = [-1 -1 0 0];
+    patch(xp,yp,0*ones(1,3),'FaceAlpha',0.1,'EdgeColor','none');
+
+    
+    axis equal
+    xlim([x_min x_max])
+    ylim([-0.75 2.5])
+    pause(model.h_k);
+    clf
+end
+
