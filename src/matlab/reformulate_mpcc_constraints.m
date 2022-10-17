4a4b6672c6fab039a28004f57afea6c8a78794b3
Jonathan Frey <jonathanpaulfrey@gmail.com>
2022-10-17 20:34:44 +0200

    fix indentation

--- src/matlab/reformulate_mpcc_constraints.m
+++ src/matlab/reformulate_mpcc_constraints.m
@@ -33,97 +33,97 @@ else
 %     k = current_index.k;  i = current_index.i; j = current_index.j;
     unfold_struct(current_index,'caller')
 end
-%% Initalize
- n_all_comp_j = length(g_all_comp_j); % outputdimension of the bilinear constraint.
- g_comp = [];
- g_comp_lb = [];
- g_comp_ub = [];
- %% Adding all Complementarity Constraints (standard and cross complementarity), their treatment depends on the chosen MPCC Method.
-             if ~isempty(g_all_comp_j)
-                switch mpcc_mode
-                    case 1
-                        disp('Info: this is implemented as a special case of 3, with sigma = 0');
-                        % works better with X1x2 <=0 than X1x2 = 0;
-                    case 2
-                        % smoothed  - algebraic constraints
-                        g_comp = g_all_comp_j-p(1)*ones(n_all_comp_j,1);
-                        g_comp_lb = zeros(n_all_comp_j,1);
-                        g_comp_ub = zeros(n_all_comp_j,1);
-                    case 3
-                        g_comp = g_all_comp_j-p(1)*ones(n_all_comp_j,1);
-                        g_comp_lb = -inf*ones(n_all_comp_j,1);
-                        g_comp_ub = zeros(n_all_comp_j,1);
-                    case 4
-                            if objective_scaling_direct
-                                J = J + (1/p(1))*sum(g_all_comp_j);
-                            else
-                                J = p(1)*J + sum(g_all_comp_j);
-                            end
-                    case 5
-                        % elastic mode - inequality
-                        g_comp = g_all_comp_j-s_elastic*ones(n_all_comp_j,1);
-                        g_comp_lb = -inf*ones(n_all_comp_j,1);
-                        g_comp_ub = zeros(n_all_comp_j,1);
-                    case 6
-                        % elastic mode - equality
-                        g_comp = g_all_comp_j-s_elastic*ones(n_all_comp_j,1);
-                        g_comp_lb = zeros(n_all_comp_j,1);
-                        g_comp_ub = zeros(n_all_comp_j,1);
-                    case 7
-                        % elastic mode - two sided inequality
-                        g_comp = g_all_comp_j-s_elastic*ones(n_all_comp_j,1);
-                        g_comp_ub = zeros(n_all_comp_j,1);
-                        g_comp_lb = -inf*ones(n_all_comp_j,1);
+%% Initialize
+n_all_comp_j = length(g_all_comp_j); % outputdimension of the bilinear constraint.
+g_comp = [];
+g_comp_lb = [];
+g_comp_ub = [];
+%% Adding all Complementarity Constraints (standard and cross complementarity), their treatment depends on the chosen MPCC Method.
+if ~isempty(g_all_comp_j)
+    switch mpcc_mode
+        case 1
+            disp('Info: this is implemented as a special case of 3, with sigma = 0');
+            % works better with X1x2 <=0 than X1x2 = 0;
+        case 2
+            % smoothed  - algebraic constraints
+            g_comp = g_all_comp_j-p(1)*ones(n_all_comp_j,1);
+            g_comp_lb = zeros(n_all_comp_j,1);
+            g_comp_ub = zeros(n_all_comp_j,1);
+        case 3
+            g_comp = g_all_comp_j-p(1)*ones(n_all_comp_j,1);
+            g_comp_lb = -inf*ones(n_all_comp_j,1);
+            g_comp_ub = zeros(n_all_comp_j,1);
+        case 4
+            if objective_scaling_direct
+                J = J + (1/p(1))*sum(g_all_comp_j);
+            else
+                J = p(1)*J + sum(g_all_comp_j);
+            end
+        case 5
+            % elastic mode - inequality
+            g_comp = g_all_comp_j-s_elastic*ones(n_all_comp_j,1);
+            g_comp_lb = -inf*ones(n_all_comp_j,1);
+            g_comp_ub = zeros(n_all_comp_j,1);
+        case 6
+            % elastic mode - equality
+            g_comp = g_all_comp_j-s_elastic*ones(n_all_comp_j,1);
+            g_comp_lb = zeros(n_all_comp_j,1);
+            g_comp_ub = zeros(n_all_comp_j,1);
+        case 7
+            % elastic mode - two sided inequality
+            g_comp = g_all_comp_j-s_elastic*ones(n_all_comp_j,1);
+            g_comp_ub = zeros(n_all_comp_j,1);
+            g_comp_lb = -inf*ones(n_all_comp_j,1);
 
-                        g_comp = [g_comp;g_all_comp_j+s_elastic*ones(n_all_comp_j,1)];
-                        g_comp_ub = [g_comp_ub; inf*ones(n_all_comp_j,1)];
-                        g_comp_lb = [g_comp_lb;  zeros(n_all_comp_j,1)];
-                    case 8
-                        % elastic mode with barrier homotopy
-                        % elastic mode - inequality
-                        g_comp = g_all_comp_j-s_elastic*ones(n_all_comp_j,1);
-                        g_comp_lb = -inf*ones(n_all_comp_j,1);
-                        g_comp_ub = zeros(n_all_comp_j,1);
+            g_comp = [g_comp;g_all_comp_j+s_elastic*ones(n_all_comp_j,1)];
+            g_comp_ub = [g_comp_ub; inf*ones(n_all_comp_j,1)];
+            g_comp_lb = [g_comp_lb;  zeros(n_all_comp_j,1)];
+        case 8
+            % elastic mode with barrier homotopy
+            % elastic mode - inequality
+            g_comp = g_all_comp_j-s_elastic*ones(n_all_comp_j,1);
+            g_comp_lb = -inf*ones(n_all_comp_j,1);
+            g_comp_ub = zeros(n_all_comp_j,1);
 %                         lbg = [lbg; g_comp_lb];
 %                         ubg = [ubg; g_comp_ub];
-                    case 9
-                        % elastic mode with barrier homotopy elastic mode - equality
-                        g_comp = g_all_comp_j-s_elastic*ones(n_all_comp_j,1);
-                        g_comp_lb = zeros(n_all_comp_j,1);
-                        g_comp_ub = zeros(n_all_comp_j,1);
+        case 9
+            % elastic mode with barrier homotopy elastic mode - equality
+            g_comp = g_all_comp_j-s_elastic*ones(n_all_comp_j,1);
+            g_comp_lb = zeros(n_all_comp_j,1);
+            g_comp_ub = zeros(n_all_comp_j,1);
 
-                    case 10
+        case 10
 %                         if  k == N_stages-1 && j == n_s && i == N_finite_elements(end)-1
-                            if objective_scaling_direct
-                                J = J + (1/s_elastic)*sum(g_all_comp_j);
-                            else
-                                J = s_elastic*J + sum(g_all_comp_j);
-                            end
-%                         end
-                    case 11
-                          % ell_1 elastic mode - inequality
-                        g_comp = g_all_comp_j-s_elastic;
-                        g_comp_lb = -inf*ones(n_all_comp_j,1);
-                        g_comp_ub = zeros(n_all_comp_j,1);
-                        case 12
-                       % elastic mode - equality
-                        g_comp = g_all_comp_j-s_elastic;
-                        g_comp_lb = zeros(n_all_comp_j,1);
-                        g_comp_ub = zeros(n_all_comp_j,1);
-                    case 13
-                        % elastic mode - two sided inequality
-                        g_comp = g_all_comp_j-s_elastic;
-                        g_comp_ub = zeros(n_all_comp_j,1);
-                        g_comp_lb = -inf*ones(n_all_comp_j,1);
-
-                        g_comp = [g_comp;g_all_comp_j+s_elastic];
-                        g_comp_ub = [g_comp_ub; inf*ones(n_all_comp_j,1)];
-                        g_comp_lb = [g_comp_lb;  zeros(n_all_comp_j,1)];
-
-                    otherwise
-                        error('Pick mpcc_mode between 1 and 13.');
+                if objective_scaling_direct
+                    J = J + (1/s_elastic)*sum(g_all_comp_j);
+                else
+                    J = s_elastic*J + sum(g_all_comp_j);
                 end
-            end
+%                         end
+        case 11
+                % ell_1 elastic mode - inequality
+            g_comp = g_all_comp_j-s_elastic;
+            g_comp_lb = -inf*ones(n_all_comp_j,1);
+            g_comp_ub = zeros(n_all_comp_j,1);
+            case 12
+            % elastic mode - equality
+            g_comp = g_all_comp_j-s_elastic;
+            g_comp_lb = zeros(n_all_comp_j,1);
+            g_comp_ub = zeros(n_all_comp_j,1);
+        case 13
+            % elastic mode - two sided inequality
+            g_comp = g_all_comp_j-s_elastic;
+            g_comp_ub = zeros(n_all_comp_j,1);
+            g_comp_lb = -inf*ones(n_all_comp_j,1);
+
+            g_comp = [g_comp;g_all_comp_j+s_elastic];
+            g_comp_ub = [g_comp_ub; inf*ones(n_all_comp_j,1)];
+            g_comp_lb = [g_comp_lb;  zeros(n_all_comp_j,1)];
+
+        otherwise
+            error('Pick mpcc_mode between 1 and 13.');
+    end
+end
 
 end
 
