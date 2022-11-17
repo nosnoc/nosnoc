72a113465d5a19798b3e42ac9be4b6b966408f49
Anton Edvinovich Pozharskiy <apozharski@gmail.com>
2022-11-17 17:41:01 +0100

    Add basic testing using gitub actions, and an example matlab test. (#16)
    * Initial action work
    
    * add manual dispatch
    
    * also on push
    
    * untar directories
    
    * drop leading v from tag
    
    * working dir change
    
    * fix path that is added to MATLABPATH
    
    * run-command seems to be unhappy with the path I am giving it
    
    * debugging matlab command
    
    * better find
    
    * cd to folder then execute
    
    * typo
    
    * more debugging
    
    * extraction going wrong
    
    * making sure we don't extract incorrectly
    
    * this could probably be done by readins the manual but testing is more fun
    
    * tar converts everything to relative paths
    
    * WIP: towards a test suite
    
    * some basic tests
    
    * correct nosnoc path
    
    * use run-tests
    
    * remove unecessary script
    
    * add folder to prevent strange tests from running
    
    * live scripts as tests are broken up by section
    
    * cleanup and artifact upload
    
    * cleanup and moving test files inside the matlab directory
    
    * By default matlab/run-tests includes everything with a _test suffix, and each section in live scripts is treated as an independent test
    
    * better names
    
    * always upload test results, and remove some failing mpcc_modes
    
    * Rename/move per review

---
+++ test/TestSettings.m
@@ -0,0 +1,20 @@
+classdef TestSettings < matlab.unittest.TestCase
+    %TESTSETTINGS Summary of this class goes here
+    %   Detailed explanation goes here
+    properties (TestParameter)
+        cross_comp = {1,2,3};
+        mpcc_mode = {1,2,3,4,5,6,8,9}; % NOTE: 7 currently fails.
+    end
+    
+    methods (Test)
+    end
+    methods (Test, ParameterCombination = 'pairwise')
+        function test_cross_comp_modes(testCase,cross_comp,mpcc_mode)
+            import matlab.unittest.constraints.IssuesNoWarnings;
+            issuesNoWarningsConstraint = IssuesNoWarnings('WhenNargoutIs', 4);
+            testCase.verifyThat(@() test_simple_car_model(cross_comp,mpcc_mode), issuesNoWarningsConstraint);
+        end
+    end 
+end
+
+
---
+++ test/test_simple_car_model.m
@@ -0,0 +1,40 @@
+function [results,stats,model,settings] = test_simple_car_model(cross_comp, mpcc_mode)
+%TEST_SIMPLE_CAR_MODEL Test the simple car model accross cross
+%complementarity and mpcc modes
+import casadi.*
+[settings] = default_settings_nosnoc();  
+settings.irk_scheme = 'Radau-IIA';
+% set the cross complimentarity mode that 
+settings.cross_comp_mode = cross_comp;
+settings.mpcc_mode = mpcc_mode;
+settings.n_s = 2;
+% Time-settings  - Solve an time optimal control problem
+settings.time_optimal_problem = 1;
+% Model - define all problem functions and
+% Discretization parameters
+model.N_stages = 10; % number of control intervals
+model.N_finite_elements = 3; % number of finite element on every control intevral (optionally a vector might be passed)
+model.T = 1;    % Time horizon
+% Symbolic variables and bounds
+q = SX.sym('q'); v = SX.sym('v'); 
+model.x = [q;v]; % add all important data to the struct model,
+model.x0 = [0;0]; % inital value
+% bounds on states
+model.lbx = [-inf;-20];
+model.ubx = [inf;20];
+% control
+u = SX.sym('u'); model.u = u;
+model.lbu = -5; model.ubu = 5;
+% Dyanmics and the regions
+f_1 = [v;u]; % mode 1 - nominal
+f_2 = [v;3*u]; % mode 2 - turbo
+model.S = [-1;1];
+model.F = [f_1 f_2];
+model.c = v-10;
+% Add terminal constraint
+model.g_terminal = [q-200;v-0];
+% Solve OCP
+[results,stats,model,settings] = nosnoc_solver(model,settings);
+
+end
+
