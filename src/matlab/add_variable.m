45ca688688562c10024d56ffe3fd32446918a789
Anton Edvinovich Pozharskiy <apozharski@gmail.com>
2022-12-15 17:27:53 +0100

    Added constraint adding util, rename var

--- src/matlab/add_variable.m
+++ src/matlab/add_variable.m
@@ -1,10 +1,10 @@
-function [problem] = add_variable(problem, w, w0, lbw, ubw, type)
+function [problem] = add_variable(problem, w, w0, lbw, ubw, idx)
 
     problem.w = {problem.w{:}, w};
     % This is a bit of a hack, to avoid a bunch of if statements.
-    if exist('type','var')
-        ind_set = problem.(strcat('ind_', type));
-        problem.(strcat('ind_', type)) = [ind_set, length(problem.w0)+1:length(problem.w0)+length(w)];
+    if exist('idx','var')
+        ind_set = problem.(strcat('ind_', idx));
+        problem.(strcat('ind_', idx)) = [ind_set, length(problem.w0)+1:length(problem.w0)+length(w)];
     end
     problem.w0 = [problem.w0; w0];
     problem.lbw = [problem.lbw; lbw];
