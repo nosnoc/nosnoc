0fc8d5f6b347bc41fe66607cf88ddc6a0f1d17cc
Anton Pozharskiy <apozharski@gmail.com>
2022-12-15 21:21:38 +0100

    dims test

--- src/matlab/add_variable.m
+++ src/matlab/add_variable.m
@@ -1,5 +1,8 @@
 function [problem] = add_variable(problem, w, w0, lbw, ubw, idx)
-
+    dims = [length(w), length(w0), length(lbw), length(ubw)];
+    if(~all(dims == dims(1)))
+        error("dimension mismatch, with dims: w: %d, w0: %d, lbw: %d, ubw: %d", dims(1), dims(2), dims(3), dims(4));
+    end
     problem.w = {problem.w{:}, w};
     % This is a bit of a hack, to avoid a bunch of if statements.
     if exist('idx','var')
