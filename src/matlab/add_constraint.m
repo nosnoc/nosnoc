8a2aaa6441273100d07405ed5eafae0eb695c00c
Anton Pozharskiy <apozharski@gmail.com>
2022-12-15 21:11:27 +0100

    fix typo, add dims check

--- src/matlab/add_constraint.m
+++ src/matlab/add_constraint.m
@@ -1,7 +1,11 @@
 function [problem] = add_constraint(problem, g, lbg, ubg, idx)
+    dims = [length(g), length(lbg), length(ubg)];
+    if(~all(dims == dims(1)))
+        error("dimension mismatch, with dims: g: %d, lbg: %d, ubg: %d", dims(1), dims(2), dims(3));
+    end
     problem.g = {problem.g{:}, g};
     problem.lbg = [problem.lbg; lbg];
-    problem.ubg = [problem.lbg; ubg];
+    problem.ubg = [problem.ubg; ubg];
     % This is a bit of a hack, to avoid a bunch of if statements (maybe, currently would only be one)
     if exist('idx','var')
         ind_set = problem.(strcat('ind_', idx));
