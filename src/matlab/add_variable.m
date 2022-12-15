c57ee0753f9ac03a9e191a433c238458333453ec
Anton Edvinovich Pozharskiy <apozharski@gmail.com>
2022-12-15 16:04:09 +0100

    optional index parameter

--- src/matlab/add_variable.m
+++ src/matlab/add_variable.m
@@ -2,8 +2,10 @@ function [problem] = add_variable(problem, w, w0, lbw, ubw, type)
 
     problem.w = {problem.w{:}, w};
     % This is a bit of a hack, to avoid a bunch of if statements.
-    ind_set = problem.(strcat('ind_', type));
-    problem.(strcat('ind_', type)) = [ind_set, length(problem.w0)+1:length(problem.w0)+length(w)];
+    if exist('type','var')
+        ind_set = problem.(strcat('ind_', type));
+        problem.(strcat('ind_', type)) = [ind_set, length(problem.w0)+1:length(problem.w0)+length(w)];
+    end
     problem.w0 = [problem.w0; w0];
     problem.lbw = [problem.lbw; lbw];
     problem.ubw = [problem.ubw; ubw];
