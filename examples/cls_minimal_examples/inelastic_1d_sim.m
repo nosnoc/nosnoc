061040a0bd0e84c63115aa939bbe9c73d54f90f8
armin.nurkanovic <armin.nurkanovic@imtek.uni-freiburg.de>
2023-04-27 16:38:08 +0200

    add 1d bouncing ball with analytic solution

--- examples/cls_minimal_examples/inelastic_1d_sim.m
+++ examples/cls_minimal_examples/inelastic_1d_sim.m
@@ -10,29 +10,19 @@ settings.n_s = 2;
 % settings.psi_fun_type = CFunctionType.STEFFENSON_ULBRICH;
 settings.print_level = 2;
 settings.N_homotopy = 6;
-settings.cross_comp_mode = 6;
+settings.cross_comp_mode = 1;
 settings.dcs_mode = DcsMode.CLS;
-settings.time_freezing = 0; %% we will need to exlude the coexistence of these two
 settings.friction_model = "Conic"; % "Polyhedral"
 settings.conic_model_switch_handling = "Abs";  % Plain % Lp
-settings.local_speed_of_time_variable = 0;
-settings.use_speed_of_time_variables = 0;
-% if settings.time_freezing && settings.dcs_mode~=DcsMode.CLS
-% settings.impose_terminal_phyisical_time = 1;
-% settings.local_speed_of_time_variable = 1;
-% settings.stagewise_clock_constraint = 0;
-% settings.pss_lift_step_functions = 0;
-% else
-%     settings.time_freezing = 1;
-% end
+
 %%
-g = 10;
+g = 9.81;
 % Symbolic variables and bounds
 q = SX.sym('q',1);
 v = SX.sym('v',1);
-model.M = diag([1]);
+model.M = 1;
 model.x = [q;v];
-model.e = 0;
+model.e = 1;
 model.mu = 0;
 model.a_n = 20;
 model.x0 = [0.2;0];
@@ -40,8 +30,8 @@ model.f_v = -g;
 model.f_c = q;
 
 %% Simulation setings
-N_FE = 2;
-T_sim = 0.5;
+N_FE = 20;
+T_sim = 0.45;
 N_sim = 1;
 model.T_sim = T_sim;
 model.N_FE = N_FE;
@@ -53,21 +43,45 @@ settings.use_previous_solution_as_initial_guess = 1;
 unfold_struct(results,'base');
 qx = x_res(1,:);
 vx = x_res(2,:);
+
+
+t_s = sqrt(2*model.x0(1)/g);
+tt1 = linspace(0,t_s,100);
+tt2 = linspace(t_s,T_sim,100);
+v1 = model.x0(2)-g*tt1;
+q1 = model.x0(1)+model.x0(2)*tt1-g*tt1.^2/2;
+if model.e == 0
+    v2 = 0*(tt2-t_s);
+    q2 = 0*(tt2-t_s);
+else
+    v2 = -model.e*v1(end)-g*(tt2-t_s);
+    q2 = q1(end)+v2(1)*(tt2-t_s)-g*(tt2-t_s).^2/2;
+end
 %t_opt = x_res(5,:);
 t_opt = t_grid;
 figure
 subplot(121)
-plot(t_opt,qx);
+plot(t_grid,qx);
+hold on
+plot(tt1,q1,'k')
+plot(tt2,q2,'k')
 axis equal
 grid on
-xlabel('$q_x$','interpreter','latex');
-ylabel('$t$','interpreter','latex');
+ylabel('$q_x$','interpreter','latex');
+xlabel('$t$','interpreter','latex');
 % axis equal
 subplot(122)
-plot(t_opt,vx);
+plot(t_grid,vx);
+hold on
+plot(t_grid,vx,'bo');
+plot(tt1,v1,'k')
+plot(tt2,v2,'k')
+ylim([-3 3])
 grid on
 xlabel('$t$','interpreter','latex');
 ylabel('$v$','interpreter','latex');
 
-% [qx(end),qy(end),t_opt(end)]
+%%
+model.problem.print
+print_casadi_matrix([model.g])
 
