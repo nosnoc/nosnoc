d9a14ce998ba0121008e15a4fe458c2efa775719
armin.nurkanovic <armin.nurkanovic@imtek.uni-freiburg.de>
2023-05-02 11:36:39 +0200

    update minimal examples

--- examples/cls_minimal_examples/inelastic_2d_sim.m
+++ examples/cls_minimal_examples/inelastic_2d_sim.m
@@ -6,20 +6,21 @@ close all
 %%
 settings = NosnocOptions();
 settings.irk_scheme = IRKSchemes.RADAU_IIA;
+% settings.irk_scheme = IRKSchemes.GAUSS_LEGENDRE;
+settings.n_s = 2;
 settings.print_level = 3;
-settings.sigma_0 = 1e-2;
 settings.N_homotopy = 6;
-settings.cross_comp_mode = 1;
+settings.cross_comp_mode = 3;
 settings.dcs_mode = DcsMode.CLS;
 settings.time_freezing = 0; %% we will need to exlude the coexistence of these two
-settings.friction_model = "Conic";
+settings.friction_model = "Polyhedral";
+%settings.friction_model = "Conic"; % "Conic"
 settings.conic_model_switch_handling = "Lp";
 settings.local_speed_of_time_variable = 0;
 settings.use_speed_of_time_variables = 0;
+settings.mpcc_mode = MpccMode.elastic_ineq;
 % settings.nlpsol = 'snopt';
-settings.psi_fun_type = CFunctionType.KANZOW_SCHWARTZ;
-
-
+% settings.psi_fun_type = CFunctionType.CHEN_CHEN_KANZOW;
 %%
 g = 10;
 % Symbolic variables and bounds
@@ -28,14 +29,18 @@ v = SX.sym('v',2);
 model.M = diag([1,1]);
 model.x = [q;v];
 model.e = 0;
-model.mu = 0.0;
-model.x0 = [0;1.0;3;0];
-model.f_v = [0;-g];
+model.mu = 0.4;
+model.a_n = 20;
+model.x0 = [0;0.2;4;0];
+% % model.x0 = [0;0.0;-0.6;0];
+% model.x0 = [0;0.0;+0.6;0];
+model.f_v = [3*1;-g];
 model.f_c = q(2);
 model.J_tangent = [1; 0];
 model.D_tangent = [1,-1;0,0];
+model.n_dim_contact = 2; % TODO: REMOVE THIS IN time-freezing
 %% Simulation setings
-N_FE = 3;
+N_FE = 2;
 T_sim = 0.7;
 N_sim = 1;
 model.T_sim = T_sim;
@@ -50,8 +55,7 @@ qx = x_res(1,:);
 qy = x_res(2,:);
 vx = x_res(3,:);
 vy = x_res(4,:);
-t_opt = t_grid;
-figure
+figure(1)
 subplot(121)
 plot(qx,qy);
 axis equal
@@ -60,68 +64,34 @@ xlabel('$q_x$','interpreter','latex');
 ylabel('$q_y$','interpreter','latex');
 % axis equal
 subplot(122)
-plot(t_opt,vy);
+plot(t_grid,vy);
 hold on
-plot(t_opt,vx);
+plot(t_grid,vx);
 % axis equal
 grid on
 xlabel('$t$','interpreter','latex');
 ylabel('$v$','interpreter','latex');
-
-% [qx(end),qy(end),t_opt(end)]
-
-%%
-alpha1 = alpha_res(1,:);
-alpha2 = alpha_res(2,:);
-alpha3 = alpha_res(3,:);
-theta1 = alpha1+(1-alpha1).*(alpha2);
-alpha_aux = (1-alpha1).*(1-alpha2);
-theta2 = alpha_aux.*(1-alpha3);
-theta3 = alpha_aux.*(alpha3);
-figure;
-subplot(131)
-plot(t_grid,[theta1,nan])
-xlabel('$\tau$','interpreter','latex');
-ylabel(['$\theta_1$'],'interpreter','latex');
-grid on
-ylim([-0.1 1.1]);
-subplot(132)
-plot(t_grid,[theta2,nan])
-xlabel('$\tau$','interpreter','latex');
-ylabel(['$\theta_2$'],'interpreter','latex');
-grid on
-ylim([-0.1 1.1]);
-subplot(133)
-plot(t_grid,[theta3,nan])
-xlabel('$\tau$','interpreter','latex');
-ylabel(['$\theta_3$'],'interpreter','latex');
-grid on
-ylim([-0.1 1.1]);
-%% speed of time
+%% forces
 figure
-subplot(121)
-plot(t_grid,t_opt)
+subplot(311)
+plot(t_grid,results.all_res.x_opt(3:4,:));
 hold on
-plot(t_grid,t_grid,'k--')
 grid on
-xlabel('$\tau$','interpreter','latex');
-ylabel('$t$','interpreter','latex');
-subplot(122)
-stairs(s_sot_res)
+xlabel('$t$','interpreter','latex');
+ylabel('$v$','interpreter','latex');
+subplot(312)
+plot(t_grid,[nan*ones(model.n_contacts,1),results.all_res.lambda_normal_opt])
+hold on
+plot(t_grid,[nan*ones(model.n_tangents,1),results.all_res.lambda_tangent_opt])
 grid on
-xlabel('simulation step','interpreter','latex');
-ylabel('$s$','interpreter','latex');
+xlabel('$t$','interpreter','latex');
+ylabel('$\lambda$','interpreter','latex');
+subplot(313)
+stem(t_grid,[results.all_res.Lambda_normal_opt,nan*ones(model.n_contacts,1)])
+hold on
+stem(t_grid,[results.all_res.Lambda_tangent_opt,nan*ones(model.n_tangents,1)]')
+grid on
+xlabel('$t$','interpreter','latex');
+ylabel('$\Lambda$','interpreter','latex');
+xlim([0 T_sim])
 
-%% complementarity residuals
-if 0
-    lambda0 = lambda_0_res;
-    lambda1 = lambda_1_res;
-    alpha = alpha_res;
-    comp1 = alpha.*lambda0;
-    comp2 = lambda1.*(ones(size(alpha))-alpha);
-    figure
-    subplot(121)
-    plot(comp1')
-    subplot(122)
-    plot(comp2')
-end
--- examples/cls_minimal_examples/two_simple_contacts.m
+++ examples/cls_minimal_examples/two_simple_contacts.m
@@ -6,41 +6,30 @@ close all
 %%
 settings = NosnocOptions();
 settings.irk_scheme = IRKSchemes.RADAU_IIA;
-% settings.irk_scheme = IRKSchemes.GAUSS_LEGENDRE;
 settings.n_s = 2;
 settings.print_level = 3;
 settings.N_homotopy = 6;
 settings.cross_comp_mode = 3;
 settings.dcs_mode = DcsMode.CLS;
-settings.time_freezing = 0; %% we will need to exlude the coexistence of these two
 settings.friction_model = "Polyhedral";
 %settings.friction_model = "Conic"; % "Conic"
 settings.conic_model_switch_handling = "Lp";
-settings.local_speed_of_time_variable = 0;
-settings.use_speed_of_time_variables = 0;
-settings.mpcc_mode = MpccMode.elastic_ineq;
-% settings.nlpsol = 'snopt';
-% settings.psi_fun_type = CFunctionType.CHEN_CHEN_KANZOW;
+settings.mpcc_mode = MpccMode.Scholtes_ineq;
 %%
-g = 10;
-% Symbolic variables and bounds
+g = 9.81;
 q = SX.sym('q',2);
 v = SX.sym('v',2);
 model.M = diag([1,1]);
 model.x = [q;v];
-model.e = 0;
-model.mu = 0.4;
-model.a_n = 20;
-model.x0 = [0;0.2;4;0];
-% % model.x0 = [0;0.0;-0.6;0];
-% model.x0 = [0;0.0;+0.6;0];
+model.e = [1;0];
+model.mu = [0.4;0.7];
+model.x0 = [0.2;0.3;-2;0];
 model.f_v = [3*1;-g];
-model.f_c = q(2);
-model.J_tangent = [1; 0];
-model.D_tangent = [1,-1;0,0];
-model.n_dim_contact = 2; % TODO: REMOVE THIS IN time-freezing
+model.f_c = [q(1);q(2)];
+model.J_tangent = [0 1;1 0];
+model.D_tangent = [model.J_tangent -model.J_tangent] ;
 %% Simulation setings
-N_FE = 2;
+N_FE = 5;
 T_sim = 0.7;
 N_sim = 1;
 model.T_sim = T_sim;
@@ -60,6 +49,11 @@ subplot(121)
 plot(qx,qy);
 axis equal
 grid on
+hold on
+xline(0,'k')
+yline(0,'k')
+xlim([-0.2 1])
+ylim([-0.2 1])
 xlabel('$q_x$','interpreter','latex');
 ylabel('$q_y$','interpreter','latex');
 % axis equal
@@ -67,7 +61,6 @@ subplot(122)
 plot(t_grid,vy);
 hold on
 plot(t_grid,vx);
-% axis equal
 grid on
 xlabel('$t$','interpreter','latex');
 ylabel('$v$','interpreter','latex');
@@ -87,7 +80,7 @@ grid on
 xlabel('$t$','interpreter','latex');
 ylabel('$\lambda$','interpreter','latex');
 subplot(313)
-stem(t_grid,[results.all_res.Lambda_normal_opt,nan*ones(model.n_contacts,1)])
+stem(t_grid,[results.all_res.Lambda_normal_opt,nan*ones(model.n_contacts,1)]')
 hold on
 stem(t_grid,[results.all_res.Lambda_tangent_opt,nan*ones(model.n_tangents,1)]')
 grid on
