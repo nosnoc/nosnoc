c96fa8210b7d231a60079bfef3170acd1e75e85b
armin.nurkanovic <armin.nurkanovic@imtek.uni-freiburg.de>
2023-05-03 11:10:38 +0200

    preparing experiments

--- examples/quadroped_cls_ocp/quadroped_looper.m
+++ examples/quadroped_cls_ocp/quadroped_looper.m
@@ -1,11 +1,12 @@
-
-T = {2,2,3,3,2.5};
-N_FE = {2,3,2,2,2};
-N_stg = {20,20,30,20,25};
-max_iter = {1e3,1e3,1e3,1e3,1e3};
-cross_comp_mode = {1,1,1,1,3};
-q_x_final = {2,2,2,3,2.5};
-IrkScheme = {IRKSchemes.RADAU_IIA,IRKSchemes.GAUSS_LEGENDRE,IRKSchemes.RADAU_IIA,IRKSchemes.RADAU_IIA,IRKSchemes.RADAU_IIA};
+n_q = 7;
+n_u = 4;
+T = {3,4,3,2.5,5,6,3,4};
+N_FE = {2,2,2,2,2,2,3,2};
+N_stg = {20,30,30,40,40,25,40,30};
+max_iter = {3e3,4e3,3e3,3e3,3e3,5e3,2e3,3e3};
+cross_comp_mode = {1,1,1,1,1,1,1,1};
+q_x_final = {3,3,3,3,3,3,3,3};
+IrkScheme = {IRKSchemes.RADAU_IIA,IRKSchemes.RADAU_IIA,IRKSchemes.RADAU_IIA,IRKSchemes.RADAU_IIA,IRKSchemes.RADAU_IIA,IRKSchemes.RADAU_IIA,IRKSchemes.RADAU_IIA,IRKSchemes.RADAU_IIA};
 
 scenario.Q = diag([50; 10;  10; 10*ones(n_q-3,1); 0.1*ones(n_q,1)]);
 scenario.Q_terminal = diag([500; 500; 500; 100*ones(n_q-3,1); 50*ones(n_q,1)]);
@@ -17,7 +18,7 @@ for ii = 1:length(T)
     scenario.T = T{ii};
     scenario.N_FE = N_FE{ii};
     scenario.N_stg = N_stg{ii};
-    scenario.filename = ['quad_half_take' num2str(ii)];
+    scenario.filename = ['quad_half_iters' num2str(ii)];
     scenario.max_iter = max_iter{ii};
     scenario.cross_comp_mode = cross_comp_mode{ii};
     scenario.q_x_final = q_x_final{ii};
