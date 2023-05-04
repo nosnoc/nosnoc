7318772c12fe2b0e08c7265a007da9ed1c57f331
Anton Pozharskiy <apozharski@gmail.com>
2023-05-04 10:51:49 +0200

    wip

--- src/NosnocSolver.m
+++ src/NosnocSolver.m
@@ -49,61 +49,157 @@ classdef NosnocSolver < handle
                 end
             end
         end
+
         
-        function varargout = solve(obj)
+        function [results,stats] = solve(obj)
+            import casadi.*
             solver = obj.solver;
             model = obj.model;
             settings = obj.settings;
             solver_initialization = obj.solver_initialization
+
+
+
+
+
+
+            comp_res = model.comp_res;
+            nabla_J_fun = model.nabla_J_fun;
+            s_elastic_iter = 1;
+
+            sigma_k = sigma_0;
+            x0 = solver_initialization.w0(1:model.dimensions.n_x);
+
+            p_val = obj.getInitialParameters(x0);
+            complementarity_stats = [full(comp_res(w0, p_val))];
+
+            cpu_time = [];
+            homotopy_iterations = [];
+            w0_base = w0;
+            W = [w0];
+
+            lbw_h = solver_initialization.lbw; ubw_h = solver_initialization.ubw;
+            lbw_h(model.ind_h) = model.h_k(1);
+            ubw_h(model.ind_h) = model.h_k(1);
+
+            %% homotopy loop
+            complementarity_iter = 1;
+            ii = 0;
+
+            if print_level >= 3
+                %     fprintf('\niter\t\tsigma\t\tcompl_res\tobjective\tCPU time\tNLP iters\tstatus \t inf_pr \t inf_du \n')
+                fprintf('\niter \t\t sigma \t\t compl_res \t\t objective \t\t inf_pr \t\t inf_du \t\t CPU time \t\t NLP iters \t\t status \n')
+            end
+
+            while (complementarity_iter) > comp_tol && ii < N_homotopy && (sigma_k > sigma_N || ii == 0)
+                % homotopy parameter update
+                if ii == 0
+                    sigma_k = sigma_0;
+                else
+                    if isequal(homotopy_update_rule,'linear')
+                        sigma_k = homotopy_update_slope*sigma_k;
+                    elseif isequal(homotopy_update_rule,'superlinear')
+                        sigma_k = max(sigma_N,min(homotopy_update_slope*sigma_k,sigma_k^homotopy_update_exponent));
+                    else
+                        error('For the homotopy_update_rule please select ''linear'' or ''superlinear''.')
+                    end
+                end
+                p_val(1) = sigma_k;
+                if iscell(solver)
+                    tic
+                    results = solver{ii+1}('x0', w0, 'lbx', lbw, 'ubx', ubw,'lbg', lbg, 'ubg', ubg,'p',p_val);
+                    cpu_time_iter = toc ;
+                    stats = solver{ii+1}.stats;
+                else
+                    tic
+                    results = solver('x0', w0, 'lbx', lbw, 'ubx', ubw,'lbg', lbg, 'ubg', ubg,'p',p_val);
+                    cpu_time_iter = toc ;
+                    stats = solver.stats;
+                end
+                
+                
+                if isequal(stats.return_status,'Infeasible_Problem_Detected')
+                    obj.printInfeasibility(results);
+                end
+
+                cpu_time = [cpu_time,cpu_time_iter];
+                w_opt = full(results.x);
+                w0 = w_opt;
+                W = [W,w_opt]; % all homotopy iterations
+
+                % complementarity
+                complementarity_iter = full(comp_res(w_opt, p_val));
+                complementarity_stats = [complementarity_stats;complementarity_iter];
+                objective = full(model.problem.objective_fun(w_opt, p_val));
+                % update counter
+                ii = ii+1;
+                % Verbose
+                if print_level >= 3
+                    if strcmp(settings.nlpsol, 'ipopt')
+                        inf_pr = solver.stats.iterations.inf_pr(end);
+                        inf_du = solver.stats.iterations.inf_du(end);
+                        %             fprintf('%d\t\t%2.2e\t%2.2e\t%.3f\t\t%.3f\t\t%d\t\t%s\t\t%6.2e \t\t%6.2e\n',ii, sigma_k, complementarity_iter, objective,...
+                        %                 cpu_time_iter, stats.iter_count, stats.return_status,inf_pr,inf_du);
+                        fprintf('%d \t\t %6.2e \t\t %6.2e \t\t %6.3f \t\t %6.2e \t\t %6.2e \t\t %6.3f \t\t %d \t\t %s \n',ii, sigma_k, complementarity_iter, objective,inf_pr,inf_du, ...
+                            cpu_time_iter, stats.iter_count, stats.return_status);
+                    elseif strcmp(settings.nlpsol, 'snopt')
+                        % TODO: Findout snopt prim du inf log!
+                        inf_pr = nan;
+                        inf_du = nan;
+                        %             fprintf('%d\t\t%2.2e\t%2.2e\t%.3f\t\t%.3f\t\t\t%s\t%s\n',ii, sigma_k, complementarity_iter, objective,...
+                        %                 cpu_time_iter, stats.return_status, stats.secondary_return_status);
+                        fprintf('%d \t\t %6.2e \t\t %6.2e \t\t %6.3f \t\t %6.2e \t\t %6.2e \t\t %6.3f \t\t %d \t\t %s \n',ii, sigma_k, complementarity_iter, objective,inf_pr,inf_du, ...
+                            cpu_time_iter, stats.secondary_return_status, stats.return_status);
+                        error('todo: add missing log information')
+                    end
+                end
+            end
+
+            %% polish homotopy solution with fixed active set.
+            if polishing_step
+                [results] = polishing_homotopy_solution(model,settings,results,sigma_k);
+                complementarity_iter = results.complementarity_iter;
+                complementarity_stats = [complementarity_stats;complementarity_iter];
+                W = [W,results.w_opt];
+            end
+
+            %% collect stats
+            results.W = W;
+            stats.complementarity_stats = complementarity_stats;
+            stats.cpu_time = cpu_time;
+            stats.cpu_time_total = sum(cpu_time);
+            stats.sigma_k = sigma_k;
+            stats.homotopy_iterations = ii;
+
+
+
+
+
+
+
             [results,stats,solver_initialization] = homotopy_solver(solver,model,settings,solver_initialization);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
             total_time = sum(stats.cpu_time);
             results = extract_results_from_solver(model,settings,results);
 
-            % get 00 values
-            % TODO: Make function for parameters 00
-            lambda_00 = [];
-            gamma_00 = [];
-            p_vt_00 = [];
-            n_vt_00  = [];
-            gamma_d00 = [];
-            delta_d00 = [];
-            y_gap00 = [];
-            if settings.dcs_mode == 'CLS'
-                % TODO: reconsider this if 0th element has an impulse
-                y_gap00 = model.f_c_fun(x0);
-                if model.friction_exists
-                    switch settings.friction_model
-                        case 'Polyhedral'
-                            v0 = x0(model.dimensions.n_q+1:end);
-                            D_tangent_0 = model.D_tangent_fun(x0);
-                            v_t0 = D_tangent_0'*v0;
-                            for ii = 1:model.dimensions.n_contacts
-                                ind_temp = model.dimensions.n_t*ii-(model.dimensions.n_t-1):model.dimensions.n_t*ii;
-                                gamma_d00 = [gamma_d00;norm(v_t0(ind_temp))/model.dimensions.n_t];
-                                delta_d00 = [delta_d00;D_tangent_0(:,ind_temp)'*v0+gamma_d00(ii)];
-                            end
-                        case 'Conic'
-                            v0 = x0(model.dimensions.n_q+1:end);
-                            v_t0 = model.J_tangent_fun(x0)'*v0;
-                            for ii = 1:model.dimensions.n_contacts
-                                ind_temp = model.dimensions.n_t*ii-(model.dimensions.n_t-1):model.dimensions.n_t*ii;
-                                v_ti0 = v0(ind_temp);
-                                gamma_00 = [gamma_00;norm(v_ti0)];
-                                switch settings.conic_model_switch_handling
-                                    case 'Plain'
-                                        % no extra vars
-                                    case {'Abs','Lp'}
-                                        p_vt_00 = [p_vt_00;max(v_ti0,0)];
-                                        n_vt_00 = [n_vt_00;max(-v_ti0,0)];
-                                end
-                            end
-                    end
-                end
-            else
-                lambda_00 = full(model.lambda00_fun(x0,model.p_global_val));
-            end
-            p_comp = [model.p_val;x0;lambda_00(:);y_gap00(:);gamma_00(:);gamma_d00(:);delta_d00(:);p_vt_00(:);n_vt_00(:)];
-            complementarity_iter_ell_inf = full(comp_res(results.w_opt,p_comp));
+            
+            complementarity_iter_ell_inf = full(comp_res(results.w_opt,p_val));
             switch dcs_mode
                 case 'Step'
                     temp = [results.alpha_opt_extended.*results.lambda_n_opt_extended,(1-results.alpha_opt_extended).*results.lambda_p_opt_extended];
@@ -146,7 +242,91 @@ classdef NosnocSolver < handle
             varargout{3} = model;
             varargout{4} = settings;
             varargout{5} = solver_initialization;
-        end 
+        end
+
+        function printInfeasibility(obj, results)
+            warning('nosnoc:homotopy_solver:NLP_infeasible', 'NLP infeasible: try different mpcc_mode or check problem functions.');
+            if obj.settings.print_details_if_infeasible
+                print_problem_details(results,obj.model,obj.solver_initialization,[]);
+            end
+            if obj.settings.pause_homotopy_solver_if_infeasible
+                %             error('nosnoc: infeasible problem encounterd - stopping for debugging.')
+                keyboard
+            end
+        end
+
+        function p_val = getInitialParameters(obj, x0)
+        % cont algebraics initialization
+            model = obj.model;
+            settings = obj.settings;
+            
+            lambda00 = [];
+            gamma_00 = [];
+            p_vt_00 = [];
+            n_vt_00  = [];
+            gamma_d00 = [];
+            delta_d00 = [];
+            y_gap00 = [];
+            switch settings.dcs_mode
+                case 'Stewart'
+                    lambda00 = full(model.lambda00_fun(x0, model.p_global_val));
+                case 'Step'
+                    lambda00 = full(model.lambda00_fun(x0, model.p_global_val));
+                case 'CLS'
+                    % TODO: reconsider this if 0th element has an impulse
+                    y_gap00 = model.f_c_fun(x0);
+                    if model.friction_exists
+                        switch settings.friction_model
+                            case 'Polyhedral'
+                                v0 = x0(model.dimensions.n_q+1:end);
+                                D_tangent_0 = model.D_tangent_fun(x0);
+                                v_t0 = D_tangent_0'*v0;
+                                for ii = 1:model.dimensions.n_contacts
+                                    ind_temp = model.dimensions.n_t*ii-(model.dimensions.n_t-1):model.dimensions.n_t*ii;
+                                    gamma_d00 = [gamma_d00;norm(v_t0(ind_temp))/model.dimensions.n_t];
+                                    delta_d00 = [delta_d00;D_tangent_0(:,ind_temp)'*v0+gamma_d00(ii)];
+                                end
+                            case 'Conic'
+                                v0 = x0(model.dimensions.n_q+1:end);
+                                v_t0 = model.J_tangent_fun(x0)'*v0;
+                                for ii = 1:model.dimensions.n_contacts
+                                    ind_temp = model.dimensions.n_t*ii-(model.dimensions.n_t-1):model.dimensions.n_t*ii;
+                                    v_ti0 = v0(ind_temp);
+                                    gamma_00 = [gamma_00;norm(v_ti0)];
+                                    switch settings.conic_model_switch_handling
+                                        case 'Plain'
+                                            % no extra vars
+                                        case {'Abs','Lp'}
+                                            p_vt_00 = [p_vt_00;max(v_ti0,0)];
+                                            n_vt_00 = [n_vt_00;max(-v_ti0,0)];
+                                    end
+                                end
+                        end
+                    end
+            end
+            p_val = [model.p_val(:);x0(:);lambda00(:);y_gap00(:);gamma_00(:);gamma_d00(:);delta_d00(:);p_vt_00(:);n_vt_00(:)];
+        end
+
+        function printNLPIterInfo(obj, results, stats)
+            if strcmp(obj.settings.nlpsol, 'ipopt')
+                inf_pr = obj.solver.stats.iterations.inf_pr(end);
+                inf_du = obj.solver.stats.iterations.inf_du(end);
+                %             fprintf('%d\t\t%2.2e\t%2.2e\t%.3f\t\t%.3f\t\t%d\t\t%s\t\t%6.2e \t\t%6.2e\n',ii, sigma_k, complementarity_iter, objective,...
+                %                 cpu_time_iter, stats.iter_count, stats.return_status,inf_pr,inf_du);
+                fprintf('%d \t\t %6.2e \t\t %6.2e \t\t %6.3f \t\t %6.2e \t\t %6.2e \t\t %6.3f \t\t %d \t\t %s \n',ii, sigma_k, complementarity_iter, objective,inf_pr,inf_du, ...
+                    cpu_time_iter, stats.iter_count, stats.return_status);
+            elseif strcmp(settings.nlpsol, 'snopt')
+                % TODO: Findout snopt prim du inf log!
+                inf_pr = nan;
+                inf_du = nan;
+                %             fprintf('%d\t\t%2.2e\t%2.2e\t%.3f\t\t%.3f\t\t\t%s\t%s\n',ii, sigma_k, complementarity_iter, objective,...
+                %                 cpu_time_iter, stats.return_status, stats.secondary_return_status);
+                fprintf('%d \t\t %6.2e \t\t %6.2e \t\t %6.3f \t\t %6.2e \t\t %6.2e \t\t %6.3f \t\t %d \t\t %s \n',ii, sigma_k, complementarity_iter, objective,inf_pr,inf_du, ...
+                    cpu_time_iter, stats.secondary_return_status, stats.return_status);
+                error('todo: add missing log information')
+            end
+        end
+        
     end
 end
 
