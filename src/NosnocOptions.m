0a2d37559ea7c59237f321c386459e795057b594
Jonathan Frey <jonathanpaulfrey@gmail.com>
2023-05-04 09:19:46 +0200

    dimensions -> dims

--- src/NosnocOptions.m
+++ src/NosnocOptions.m
@@ -235,7 +235,7 @@ classdef NosnocOptions < handle
         function [] = create_butcher_tableu(obj, model)
             switch obj.irk_representation
               case IrkRepresentation.integral
-                [B, C, D, tau_root] = generate_butcher_tableu_integral(model.dimensions.n_s, obj.irk_scheme);
+                [B, C, D, tau_root] = generate_butcher_tableu_integral(model.dims.n_s, obj.irk_scheme);
                 if tau_root(end) == 1
                     right_boundary_point_explicit  = 1;
                 else
@@ -245,7 +245,7 @@ classdef NosnocOptions < handle
                 obj.C_irk = C;
                 obj.D_irk = D;
               case {IrkRepresentation.differential, IrkRepresentation.differential_lift_x}
-                [A_irk,b_irk,c_irk,order_irk] = generate_butcher_tableu(model.dimensions.n_s,obj.irk_scheme);
+                [A_irk,b_irk,c_irk,order_irk] = generate_butcher_tableu(model.dims.n_s,obj.irk_scheme);
                 if c_irk(end) <= 1+1e-9 && c_irk(end) >= 1-1e-9
                     right_boundary_point_explicit  = 1;
                 else
