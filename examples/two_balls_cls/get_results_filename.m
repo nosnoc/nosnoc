88b52a0d9caca7d1949c66a0222b088f937ae800
Jonathan Frey <jonathanpaulfrey@gmail.com>
2023-05-17 15:11:12 +0200

    make results folder

--- examples/two_balls_cls/get_results_filename.m
+++ examples/two_balls_cls/get_results_filename.m
@@ -1,7 +1,12 @@
 function results_filename = get_results_filename(n_s, N_sim, N_FE, irk_scheme, with_guess)
-    % results_filename = strcat("two_balls_ns_", num2str(n_s), '_Nsim_', num2str(N_sim),...
-    %                     "_NFE_", num2str(N_FE), '_', string(irk_scheme), '_init_', num2str(with_guess));
+    results_folder = 'results';
+    if ~exists(results_folder, 7)
+        mkdir(results_folder)
+    end
 
-    results_filename = strcat("ex_", num2str(n_s), '_', num2str(N_sim),...
-                        "_", num2str(N_FE), '_', string(irk_scheme), '_', num2str(with_guess));
+    results_filename = fullfile(results_folder, strcat("two_balls_ns_", num2str(n_s), '_Nsim_', num2str(N_sim),...
+                        "_NFE_", num2str(N_FE), '_', string(irk_scheme), '_init_', num2str(with_guess)));
+
+    % results_filename = strcat("ex_", num2str(n_s), '_', num2str(N_sim),...
+    %                     "_", num2str(N_FE), '_', string(irk_scheme), '_', num2str(with_guess));
 end
