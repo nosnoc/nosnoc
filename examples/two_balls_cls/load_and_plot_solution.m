c27aba55f5b3e88ed839f0df846e126698f7afd5
Jonathan Frey <jonathanpaulfrey@gmail.com>
2023-05-23 15:28:05 +0200

    two_balls structuring

---
+++ examples/two_balls_cls/load_and_plot_solution.m
@@ -0,0 +1,18 @@
+close all
+for n_s = [1]
+for N_sim = [94, 997]
+    irk_scheme = IRKSchemes.GAUSS_LEGENDRE;
+    with_guess = 0;
+    N_FE = 2;
+    
+    results_filename = get_results_filename(n_s, N_sim, N_FE, irk_scheme, with_guess);
+    load(results_filename);
+    label = strcat('n_s', num2str(n_s), ' N_sim', num2str(N_sim));
+    
+    if all(stats.converged)
+        plot_two_ball_traj(results, label);
+    else
+        disp(strcat(results_filename, ' failed.'))
+    end
+end
+end
