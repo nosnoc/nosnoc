127f00f879b5c6caec354be57a29a0b327b8f395
Jonathan Frey <jonathanpaulfrey@gmail.com>
2023-05-26 16:38:48 +0200

    fix extract_results when not using FESD

--- src/extract_results_from_solver.m
+++ src/extract_results_from_solver.m
@@ -35,7 +35,7 @@ else
     %     T = T_opt;
     % end
     for ii = 1:settings.N_stages
-        h_opt = [h_opt,model.T/(settings.N_stages*settings.N_finite_elements(ii))*ones(1, model.settings.N_finite_elements(ii))];
+        h_opt = [h_opt,model.T/(settings.N_stages*settings.N_finite_elements(ii))*ones(1, settings.N_finite_elements(ii))];
     end
 end
 results.h = h_opt;
