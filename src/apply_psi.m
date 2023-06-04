e839849512e826e6a89cecd126fd3137f465b8ce
Anton Pozharskiy <apozharski@gmail.com>
2023-06-05 01:23:49 +0200

    fix 2 sided bug

--- src/apply_psi.m
+++ src/apply_psi.m
@@ -9,7 +9,16 @@ function result = apply_psi(comp_pairs, psi, sigma, scale)
     end
     temp = comp_pairs(:,1);
     sparsity = temp.sparsity();
-    result = psi(comp_pairs(:,1), comp_pairs(:,2), sigma*scale);
+    % TODO: for some reason casadi also wants outputs to be scalar to allow for
+    % multiple argument evaluation. This is a problem for 2-sided relaxations so
+    % here is a mild hack to allow it to still work.
+    if (size(psi(sigma,sigma,sigma),2) == 1)
+        result = psi(comp_pairs(:,1), comp_pairs(:,2), sigma*scale);
+    else
+        result = psi(comp_pairs(:,1)', comp_pairs(:,2)', sigma*scale);
+        result = reshape(result, [length(temp),2])';
+    end
+    
     % Note: This is done to maintain sparsity of the outputs. For some reason
     % matrix application of functions does not preserve sparsity which
     % is either a bug or a feature I don't understand
