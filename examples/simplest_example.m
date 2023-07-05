795c213b1378a424789ee6527cca6a2a66a0bfa3
Anton Pozharskiy <apozharski@gmail.com>
2023-07-05 10:37:46 +0200

    NLP code changes

--- examples/simplest_example.m
+++ examples/simplest_example.m
@@ -2,11 +2,10 @@ close all
 clear all
 import casadi.*
 
-settings = NosnocOptions();
-settings.n_s = 2;
-settings.dcs_mode = 'Stewart';
-settings.mpcc_mode = MpccMode.ell_1_penalty;
-settings.print_level = 3;
+problem_options = NosnocProblemOptions();
+problem_options.n_s = 2;
+problem_options.dcs_mode = 'Stewart';
+problem_options.print_level = 3;
 
 model = NosnocModel();
 x1 = SX.sym('x1');
@@ -20,16 +19,19 @@ model.F = [f_11, f_12];
 model.x0 = -1;
 
 model.T = pi/4;
-settings.N_stages = 1;
-settings.N_finite_elements = 2;
+problem_options.N_stages = 1;
+problem_options.N_finite_elements = 2;
 model.dims.n_s = 2;
 
-model.verify_and_backfill(settings);
-model.generate_variables(settings);
-model.generate_equations(settings);
-settings.preprocess();
+model.verify_and_backfill(problem_options);
+model.generate_variables(problem_options);
+model.generate_equations(problem_options);
+problem_options.preprocess();
 
-mpcc = NosnocMPCC(settings, model.dims, model);
+mpcc = NosnocMPCC(problem_options, model.dims, model);
+
+solver_options = NosnocSolverOptions();
+solver_options.preprocess();
 
 %solver = NosnocSolver(model, settings);
 %[results,stats] = solver.solve();
