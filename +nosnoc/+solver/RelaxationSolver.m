4f80739c94b7eee3a731439e4adf3d45adcc461a
Anton Edvinovich Pozharskiy <apozharski@gmail.com>
2024-03-26 17:05:40 +0100

    some more preliminary work to getting mpccsol off the ground

--- +nosnoc/+solver/RelaxationSolver.m
+++ +nosnoc/+solver/RelaxationSolver.m
@@ -1,6 +1,7 @@
 classdef RelaxationSolver < handle & matlab.mixin.indexing.RedefinesParen
     properties
         mpcc
+        nlp
         opts
         stats % struct
     end
@@ -9,6 +10,46 @@ classdef RelaxationSolver < handle & matlab.mixin.indexing.RedefinesParen
         
         function obj=RelaxationSolver(mpcc, opts)
         % TODO(@anton) move NosnocSolver building here.
+            import casadi.*
+            casadi_symbolic_mode = class(mpcc.w);
+            obj.mpcc = mpcc;
+            obj.opts = opts;
+            opts.preprocess();
+            stats = struct;
+            
+            if isa(mpcc, 'vdx.problems.Mpcc') % We can properly interleave complementarities if we get a vdx.Mpcc
+                use_vdx = true;
+                % TODO(@anton) implement this
+            else % Otherwise use vdx internally anyway but be sad about interleaving
+                use_vdx = false;
+                nlp = vdx.Problem('casadi_type', casadi_symbolic_mode);
+                nlp.w.mpcc_w(0) = {mpcc.w};
+                nlp.p.mpcc_p(0) = {mpcc.p};
+                nlp.g.mpcc_g(0) = {mpcc.g};
+                nlp.p.sigma_p(0) = {{'sigma_p', 1}, 0, inf, opts.sigma_0};
+
+                switch opts.elasticity_mode
+                  case ElasticityMode.NONE
+                    sigma = nlp.p.sigma_p(0);
+                  case ElasticityMode.ELL_INF
+                    nlp.w.s_elastic(0) = {{'s_elastic', 1}, opts.s_elastic_min, opts.s_elastic_max, s_elastic_0};
+                    sigma = nlp.w.s_elastic(0);
+                  case ElasticityMode.ELL_1
+                    n_c = size(mpcc.G);
+                    nlp.w.s_elastic(0) = {{'s_elastic', n_c}, opts.s_elastic_min, opts.s_elastic_max, s_elastic_0};
+                    sigma = nlp.w.s_elastic(0);
+                end
+
+                % add complementarites
+                expr = psi_fun(mpcc.G, mpcc.H, sigma);
+                [lb, ub, expr] = generate_mpcc_relaxation_bounds(expr, opts);
+                nlp.g.complementarities(0) = {expr, lb, ub};
+
+                if ~opts.assume_lower_bounds % Lower bounds on G, H, not already present in MPCC
+                   nlp.g.G_lower_bounds(0) = {mpcc.G, 0, inf};
+                   nlp.g.H_lower_bounds(0) = {mpcc.G, 0, inf};
+                end
+            end
         end
 
         function out = cat(dim,varargin)
@@ -29,7 +70,7 @@ classdef RelaxationSolver < handle & matlab.mixin.indexing.RedefinesParen
         function varargout = parenReference(obj, index_op)
         % TODO(@anton) this returns mpccresults struct from homotopy
             p = inputParser;
-            addParameter(p, 'casadi_type', 'SX');
+            addParameter(p, 'lbx', 'SX');
             parse(p, varargin{:});
         end
         
