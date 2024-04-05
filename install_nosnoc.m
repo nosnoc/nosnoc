56ffde070e3045c1f933e7d8136583f1d1499b4c
Anton Edvinovich Pozharskiy <apozharski@gmail.com>
2024-04-05 13:46:10 +0200

    fix install script

--- install_nosnoc.m
+++ install_nosnoc.m
@@ -1,6 +1,7 @@
-[nosnoc_path,~,~] = fileparts(mfilename("fullpath"))
+clear all;
+[nosnoc_path,~,~] = fileparts(mfilename("fullpath"));
 sep = pathsep;
-already_installed = contains([sep, path, sep], [s, nosnoc_path, s], 'IgnoreCase', ispc);
+already_installed = contains([sep, path, sep], [sep, nosnoc_path, sep], 'IgnoreCase', ispc);
 if ~already_installed
     addpath(nosnoc_path);
     addpath(fullfile(nosnoc_path, 'src')); % TODO: remove once we move everything to +nosnoc
