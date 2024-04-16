10d99afe660e1e654a93d7d45f552d0aa9f88616
Anton Edvinovich Pozharskiy <apozharski@gmail.com>
2024-04-16 09:46:10 +0200

    more granular install_nosnoc

--- install_nosnoc.m
+++ install_nosnoc.m
@@ -1,10 +1,18 @@
 clear all;
 [nosnoc_path,~,~] = fileparts(mfilename("fullpath"));
 sep = pathsep;
-already_installed = contains([sep, path, sep], [sep, nosnoc_path, sep], 'IgnoreCase', ispc);
-if ~already_installed
+root_already_installed = contains([sep, path, sep], [sep, nosnoc_path, sep], 'IgnoreCase', ispc)
+src_already_installed = contains([sep, path, sep], [sep, fullfile(nosnoc_path, 'src'), sep], 'IgnoreCase', ispc)
+vdx_already_installed = contains([sep, path, sep], [sep, fullfile(nosnoc_path, 'external', 'vdx'), sep], 'IgnoreCase', ispc)
+
+if ~root_already_installed
     addpath(nosnoc_path);
-    addpath(fullfile(nosnoc_path, 'src')); % TODO: remove once we move everything to +nosnoc
-    addpath(fullfile(nosnoc_path, 'external', 'vdx'));
-    savepath
 end
+if ~src_already_installed
+    addpath(fullfile(nosnoc_path, 'src')); % TODO: remove once we move everything to +nosnoc
+end
+if ~vdx_already_installed
+    addpath(fullfile(nosnoc_path, 'external', 'vdx'));
+end
+savepath
+
