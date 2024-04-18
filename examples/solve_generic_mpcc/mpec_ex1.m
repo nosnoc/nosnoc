ed8ee76f25e0b4377c1ffd5486650173ab0e855b
armin.nurkanovic <armin.nurkanovic@imtek.uni-freiburg.de>
2024-04-18 16:09:17 +0200

    fixed comment in example

--- examples/solve_generic_mpcc/mpec_ex1.m
+++ examples/solve_generic_mpcc/mpec_ex1.m
@@ -19,12 +19,12 @@ p0 = 0;
 lbx = [0;0];
 ubx = [inf;inf];
 
-mpcc_struct.x = x;
-mpcc_struct.g = [];
-mpcc_struct.p = p;
-mpcc_struct.G = x1;
-mpcc_struct.H = x2;
-mpcc_struct.f = f;
+mpcc_data.x = x;
+mpcc_data.g = [];
+mpcc_data.p = p;
+mpcc_data.G = x1;
+mpcc_data.H = x2;
+mpcc_data.f = f;
 
 mpcc_method1 = 'scholtes_ineq';
 mpcc_method2 = 'kanzow_schwartz_ineq';
@@ -35,9 +35,9 @@ mpccsol_opts.homotopy_steering_strategy = "ELL_1";
 % mpccsol_opts.homotopy_steering_strategy = "DIRECT"; 
 % the parameter relaxation/smoothing parameter sigma is steered outside the optimization , cf. Table 1 in https://arxiv.org/pdf/2312.11022.pdf
 
-solver = mpccsol('generic_mpcc', mpcc_method1, mpcc_struct, mpccsol_opts);
-solver_ks = mpccsol('generic_mpcc', mpcc_method2, mpcc_struct, mpccsol_opts);
-solver_su = mpccsol('generic_mpcc', mpcc_method3, mpcc_struct, mpccsol_opts);
+solver = mpccsol('generic_mpcc', mpcc_method1, mpcc_data, mpccsol_opts);
+solver_ks = mpccsol('generic_mpcc', mpcc_method2, mpcc_data, mpccsol_opts);
+solver_su = mpccsol('generic_mpcc', mpcc_method3, mpcc_data, mpccsol_opts);
 
 mpcc_results = solver('x0', x0,...
     'lbx', lbx,...
