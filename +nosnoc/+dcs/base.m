bf7dc26cca55a9e48e3bf295770f9c4008b445c1
Anton Edvinovich Pozharskiy <apozharski@gmail.com>
2024-05-16 12:40:56 +0200

    move shared functions to dcs base

--- +nosnoc/+dcs/base.m
+++ +nosnoc/+dcs/base.m
@@ -1,5 +1,16 @@
 classdef base < matlab.mixin.Scalar & handle
     properties
+        f_x_fun
+        f_q_fun
+        g_z_fun
+        g_alg_fun
+        g_path_fun
+        g_comp_path_fun
+        g_terminal_fun
+        f_q_T_fun
+        f_lsq_x_fun
+        f_lsq_u_fun
+        f_lsq_T_fun
     end
 
     methods(Abstract)
--- +nosnoc/+dcs/stewart.m
+++ +nosnoc/+dcs/stewart.m
@@ -18,21 +18,9 @@ classdef stewart < nosnoc.dcs.base
 
         dims
 
-        % TODO some of these should be in base perhaps
-        f_x_fun
-        f_q_fun
-        g_z_fun
-        g_alg_fun
         g_switching_fun
         g_Stewart_fun
         lambda00_fun
-        g_path_fun
-        g_comp_path_fun
-        g_terminal_fun
-        f_q_T_fun
-        f_lsq_x_fun
-        f_lsq_u_fun
-        f_lsq_T_fun
     end
 
     methods
