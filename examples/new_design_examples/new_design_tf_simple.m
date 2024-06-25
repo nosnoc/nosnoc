e38c3a2858fb56152c27aa6615d0103a8f0548e8
Anton Edvinovich Pozharskiy <apozharski@gmail.com>
2024-06-25 12:24:05 +0200

    playing with time freezing allowing terminal/numerical time relaxation

--- examples/new_design_examples/new_design_tf_simple.m
+++ examples/new_design_examples/new_design_tf_simple.m
@@ -9,18 +9,22 @@ model = nosnoc.model.Cls();
 %%
 problem_options.rk_scheme = RKSchemes.RADAU_IIA;
 problem_options.dcs_mode = DcsMode.Heaviside;
-solver_options.print_level = 3;
 problem_options.cross_comp_mode = 7;
 %problem_options.lift_complementarities = 1;
 problem_options.time_freezing = 1;
 problem_options.impose_terminal_phyisical_time = 1;
-%problem_options.local_speed_of_time_variable = 1;
 problem_options.stagewise_clock_constraint = 0;
-problem_options.s_sot_max = inf;
-problem_options.rho_sot = 0;
+problem_options.s_sot_max = 25;
+%problem_options.rho_sot = 1e-1;
 problem_options.rho_h = 1;
-problem_options.k_aux = 10;
-%problem_options.experimental_supervertical_form = 1;
+problem_options.k_aux = 100;
+problem_options.relax_terminal_physical_time = ConstraintRelaxationMode.NONE;
+problem_options.rho_terminal_physical_time = 1e4;
+%problem_options.relax_terminal_numerical_time = ConstraintRelaxationMode.NONE;
+%problem_options.rho_terminal_numerical_time = 1e3;
+%problem_options.step_equilibration = StepEquilibrationMode.linear_complementarity;
+
+solver_options.print_level = 3;
 solver_options.complementarity_tol = 1e-10;
 solver_options.sigma_N = 1e-10;
 solver_options.N_homotopy = 16;
@@ -35,7 +39,7 @@ q = SX.sym('q',2);
 v = SX.sym('v',2);
 
 model.x = [q;v]; 
-model.e = 1.0;
+model.e = 0;
 model.mu = 0.0;
 model.x0 = [0;1.0;1;0]; 
 model.f_v = [0;-g+vertical_force*g*q(1)];
@@ -44,24 +48,27 @@ model.J_tangent = [1; 0];
 model.dims.n_dim_contact = 2;
 
 %% Simulation settings
-N_FE = 3;
+N_FE = 4;
 T_sim = 1.1;
-N_sim = 100;
+N_sim = 21;
 problem_options.T_sim = T_sim;
 problem_options.N_finite_elements = N_FE;
 problem_options.N_sim = N_sim;
 problem_options.n_s = 3;
 
 integrator = nosnoc.Integrator(model, problem_options, solver_options);
-integrator.set_param('rho_physical_time', 1000);
-[t_grid, x_res] = integrator.simulate();
+[t_grid, x_res, t_grid_full, x_res_full] = integrator.simulate();
 % %
 figure
 plot(x_res(end,:)', x_res(1:2,:)')
 grid on
 xlabel('$t$','Interpreter','latex')
 ylabel('$x(t)$','Interpreter','latex')
+figure
+plot(x_res_full(end,:)', x_res_full(1:2,:)')
 grid on
+xlabel('$t$','Interpreter','latex')
+ylabel('$x(t)$','Interpreter','latex')
 figure
 plot(x_res(end,:), x_res(3:4,:))
 grid on
@@ -69,9 +76,9 @@ xlabel('$t$','Interpreter','latex')
 ylabel('$v(t)$','Interpreter','latex')
 grid on
 figure
-plot(x_res(1,:), x_res(2,:))
+plot(x_res_full(1,:), x_res_full(2,:))
 figure
-plot(x_res(2,:), x_res(4,:))
+plot(x_res_full(2,:), x_res_full(4,:))
 figure
 hold on
 plot(t_grid, x_res(end,:))
