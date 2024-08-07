f36b324a4c0b2223f80b0882a497665eb857a4f0
Anton Pozharskiy <apozharski@gmail.com>
2024-08-07 13:17:42 +0200

    estimation example

--- examples/estimator_tutorial/nosnoc_estimation_problem.m
+++ examples/estimator_tutorial/nosnoc_estimation_problem.m
@@ -1,12 +1,12 @@
 clear all
 close all
 import casadi.*
-problem_options = NosnocProblemOptions();
+problem_options = nosnoc.Options();
 solver_options = nosnoc.solver.Options();
 % Choosing the Runge - Kutta Method and number of stages
 problem_options.n_s = 2; % number of runge Kutta stages
 problem_options.dcs_mode = "Stewart";
-model = NosnocModel();
+model = nosnoc.model.Pss();
 N_stages = 50; % number of data points 
 N_FE = 2; % number of intermediate integration steps between data points
 problem_options.N_stages = N_stages; % number of control intervals, or data poitns in this context
@@ -24,33 +24,35 @@ f_2 = -1;  % for c > 0 , hence 1 in second entry of S
 model.S = [-1;1];
 model.F = [f_1 f_2]; % collect all dynamics modes in one matrix
 
-mpcc = NosnocMPCC(problem_options, model);
-solver = NosnocSolver(mpcc, solver_options);
-[results,stats] = solver.solve();
+ocp_solver = nosnoc.ocp.Solver(model, problem_options, solver_options);
+ocp_solver.solve();
+
+x_res = ocp_solver.get("x");
+t_grid = ocp_solver.get_time_grid();
 
 %%
 figure
-plot(results.t_grid,results.x)
+plot(t_grid, x_res)
 grid on
 xlabel('t')
 ylabel('x(t)')
 hold on
 %% Set up estimation problem
 % create data by perturbing simulation results with random points from [-0.1,0.1]
-dx = 0.00*1;
-x_data = results.x + (-dx + 2*dx.*rand(size(results.x)));
-plot(results.t_grid,x_data,'x')
+dx = 0.1*1;
+x_data = x_res + (-dx + 2*dx.*rand(size(x_res)));
+plot(t_grid,x_data,'x')
 x_samples = x_data(1:N_FE:end); 
 
 %% Create nosnoc estimation problem
-problem_options = NosnocProblemOptions();
+problem_options = nosnoc.Options();
 solver_options = nosnoc.solver.Options();
 % Choosing the Runge - Kutta Method and number of stages
 problem_options.n_s = 2; % number of runge Kutta stages
 problem_options.dcs_mode = "Stewart";
 problem_options.step_equilibration = 'direct';
-problem_options.cost_integration = 0;
-model = NosnocModel();
+problem_options.euler_cost_integration = 1;
+model = nosnoc.model.Pss();
 problem_options.N_stages = N_stages; % number of control intervals (no controls, set to one)
 problem_options.N_finite_elements = N_FE; % number of integration steps (in one control intevral)
 problem_options.T = 2;    % Time/simulation horizon
@@ -73,13 +75,13 @@ model.F = [f_1 f_2]; % collect all dynamics modes in one matrix
 model.p_time_var = x_data_sym;
 model.p_time_var_val = x_samples(2:end); % value of the parameters
 model.f_q = 1e3*(x-x_data_sym)'*(x-x_data_sym); % linear least squares objective
-% model.f_q_T = (x-x_data_sym)'*(x-x_data_sym); % linear least squares terminal objective
 
+estimator = nosnoc.ocp.Solver(model, problem_options, solver_options);
+estimator.solve();
 
-mpcc = NosnocMPCC(problem_options, model);
-solver = NosnocSolver(mpcc, solver_options);
-[results_estimation,stats_estimation] = solver.solve();
+x_est = estimator.get("x");
+t_grid_est = estimator.get_time_grid();
 
 %%
-plot(results_estimation.t_grid,results_estimation.x)
+plot(t_grid_est, x_est)
 
