38978979cec1518337cc89122ea4a17beb3f4e90
Anton Edvinovich Pozharskiy <apozharski@gmail.com>
2024-08-12 16:03:15 +0200

    working HomotopySolver skeleton, but still polishing left to do

--- codegen_templates/HomotopySolver.hpp
+++ codegen_templates/HomotopySolver.hpp
@@ -8,13 +8,13 @@ class HomotopySolver{
   HomotopySolver();
   uint32_t solve(std::map<std::string, DM> arg);
  private:
-  std::vector<double> lbw = {{'{' ~ nlp_lbw|join(', ') ~ '}' }};
-  std::vector<double> ubw = {{'{' ~ nlp_ubw|join(', ') ~ '}' }};
-  std::vector<double> lbg = {{'{' ~ nlp_lbg|join(', ') ~ '}' }};
-  std::vector<double> ubg = {{'{' ~ nlp_ubg|join(', ') ~ '}' }};
-  std::vector<double> p0 = {{'{' ~ nlp_p0|join(', ') ~ '}' }};
-  std::vector<double> x0 = {{'{' ~ nlp_x0|join(', ') ~ '}' }};
-  const std::vector<int> ind_mpcc = {{'{' ~ ind_mpcc|join(', ') ~ '}' }};
-  Function nlp_solver;
-  Function complemetarity_function;
+  std::vector<double> m_lbw = {{'{' ~ nlp_lbw|join(', ') ~ '}' }};
+  std::vector<double> m_ubw = {{'{' ~ nlp_ubw|join(', ') ~ '}' }};
+  std::vector<double> m_lbg = {{'{' ~ nlp_lbg|join(', ') ~ '}' }};
+  std::vector<double> m_ubg = {{'{' ~ nlp_ubg|join(', ') ~ '}' }};
+  std::vector<double> m_p0 = {{'{' ~ nlp_p0|join(', ') ~ '}' }};
+  std::vector<double> m_x0 = {{'{' ~ nlp_x0|join(', ') ~ '}' }};
+  const std::vector<int> m_ind_mpcc = {{'{' ~ ind_mpcc|join(', ') ~ '}' }};
+  Function m_nlp_solver;
+  Function m_complementarity_function;
 };
--- examples/c_solver/HomotopySolver.cpp
+++ examples/c_solver/HomotopySolver.cpp
@@ -3,32 +3,66 @@
 
 HomotopySolver::HomotopySolver()
 {
-  nlp_solver = casadi::nlpsol("solver", "ipopt", "nosnoc_solver_nlp.casadi");
-  complemetarity_function = casadi::external("comp_res", "nosnoc_solver_comp.casadi");
+  casadi::Dict nlpopts;
+  // TODO: populate all of these 
+  nlpopts["ipopt.print_level"] = 0;
+  nlpopts["print_time"] = 0;
+  nlpopts["ipopt.sb"] = "yes";
+  nlpopts["ipopt.bound_relax_factor"] = 0;
+  nlpopts["ipopt.tol"] = 1e-12;
+  m_nlp_solver = casadi::nlpsol("solver", "ipopt", "nosnoc_solver_nlp.casadi", nlpopts);
+  m_complementarity_function = casadi::external("comp_res", "nosnoc_solver_comp.casadi");
 }
 
 uint32_t HomotopySolver::solve(std::map<std::string, casadi::DM> arg)
 {
-  p0[0] = 1;
-  
-  std::map<std::string, casadi::DM> nlp_arg = {{"lbx", lbw},
-                                               {"ubx", ubw},
-                                               {"lbg", lbg},
-                                               {"ubg", ubg},
-                                               {"x0", x0},
-                                               {"p", p0}};
-  
+  m_p0.back() = 1;
 
-  auto nlpsolin = casadi::nlpsol_in();
-  for(auto str : nlpsolin)
+  
+  casadi::DM w_mpcc_res;
+  casadi::DM(m_x0).get(w_mpcc_res, false, m_ind_mpcc);
+  casadi::DM w_nlp_k = casadi::DM(m_x0);
+  DM complementarity_iter;
+  double sigma_k = 1;
+  bool last_iter_failed = false;
+  uint32_t ii = 0;
+  do
   {
-    std::cout << str << std::endl;
-  }
-  for(auto p : p0)
-  {
-    std::cout << p << ", ";
-  }
+    m_p0.back() = sigma_k;
+    
+    std::map<std::string, casadi::DM> nlp_arg = {{"lbx", m_lbw},
+                                               {"ubx", m_ubw},
+                                               {"lbg", m_lbg},
+                                               {"ubg", m_ubg},
+                                               {"x0", w_nlp_k},
+                                               {"p", m_p0}};
+    
+    auto res = m_nlp_solver(nlp_arg);
+    w_nlp_k = res.at("x");
+    w_nlp_k.get(w_mpcc_res, false, m_ind_mpcc);
+    auto p_mpcc_vec = std::vector<double>(m_p0); // TODO don't copy here for no reason
+    p_mpcc_vec.pop_back();
+    auto p_mpcc_dm = casadi::DM(p_mpcc_vec);
+    auto comp_args = {w_mpcc_res, p_mpcc_dm};
+    auto comp_fun_ret = m_complementarity_function(comp_args);
+    complementarity_iter = comp_fun_ret[0];
+    auto ret_status = m_nlp_solver.stats().at("return_status");
+    if(std::string("Solve_Succeeded").compare(ret_status) == 0 ||
+       std::string("Solved_To_Acceptable_Level").compare(ret_status) == 0||
+       std::string("Search_Direction_Becomes_Too_Small").compare(ret_status) == 0)
+    {
+      last_iter_failed = false;
+    }
+    else
+    {
+      last_iter_failed = true;
+    }
+    ii++;
+    sigma_k *= 0.1;
+  } while((complementarity_iter.get_elements()[0] > 1e-09 || last_iter_failed) &&
+          ii < 11 &&
+          sigma_k > 1.0000000000000002e-10);
 
-  auto res = nlp_solver(arg);
+  std::cout << "mpcc result w: " << w_mpcc_res.get_elements() << std::endl;
   return 0;
 }
