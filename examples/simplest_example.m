e999bab4c156806777e194a47c425600fed7e26c
Anton Edvinovich Pozharskiy <apozharski@gmail.com>
2024-08-29 14:26:25 +0200

    simplest example

--- examples/simplest_example.m
+++ examples/simplest_example.m
@@ -7,7 +7,7 @@ problem_options.n_s = 1;
 problem_options.dcs_mode = 'Stewart';
 problem_options.print_level = 1;
 
-model = NosnocModel();
+model = nosnoc.model.Pss();
 x1 = SX.sym('x1');
 model.x = x1;
 model.c = [x1];
@@ -18,7 +18,8 @@ f_12 = 1;
 model.F = [f_11, f_12];
 model.x0 = -1;
 
-problem_options.T = pi/4;
+problem_options.T_sim = pi/4;
+problem_options.N_sim = 1;
 problem_options.N_stages = 1;
 problem_options.N_finite_elements = 2;
 problem_options.cross_comp_mode = 1;
@@ -29,9 +30,8 @@ problem_options.n_s = 2;
 solver_options = nosnoc.solver.Options();
 solver_options.opts_casadi_nlp.ipopt.max_iter = 20000;
 
-mpcc = NosnocMPCC(problem_options, model);
-solver = NosnocSolver(mpcc, solver_options);
-[results,stats] = solver.solve();
+integrator = nosnoc.Integrator(model, problem_options, solver_options);
+[t_grid, x_res, t_grid_full, x_res_full] = integrator.simulate();
 
-plot(results.t_grid,results.x)
+plot(t_grid,x_res)
 grid on
