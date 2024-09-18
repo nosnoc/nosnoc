dba923b80f0771746de946fb4ade458135f960bc
Anton Edvinovich Pozharskiy <apozharski@gmail.com>
2024-09-18 18:17:00 +0200

    Implementing an MPC class which solves the full MPCC at each iteration. (#130)
    * add shift initialization and set_x0 to nosnoc.ocp.Solver
    
    * bump vdx
    
    * full mpcc mpc implementation and tutorial example
    
    * add support for x0 as argument to simulate and fix validation
    
    * modify tutorial example and add example with model plan mismatch
    
    * some exmaple polishing
    
    ---------
    
    Co-authored-by: armin.nurkanovic <armin.nurkanovic@imtek.uni-freiburg.de>

---
+++ +nosnoc/+mpc/FullMpcc.m
@@ -0,0 +1,65 @@
+classdef FullMpcc < nosnoc.mpc.Base
+% A basic MPC implementation which uses a call to the full MPCC solver to generate feedback.
+% It does either shift or in place warmstarting during the preparation phase and uses a possibly reduced
+% initial relaxation in the homotopy solver if the prior solution converged.
+    properties (Access=private)
+        ocp_solver nosnoc.ocp.Solver % nosnoc OCP solver used to generate feedback.
+        mpc_options nosnoc.mpc.Options % nosnoc mpc specific options.
+        problem_options nosnoc.Options % nosnoc problem options.
+        solver_options nosnoc.solver.Options % nosnoc solver options.
+        model % nosnoc model.
+
+        last_solve_successful(1,1) logical = false % true if last solve was successful, false otherwise.
+        cold_sigma_0(1,1) double {mustBeNonnegative} % The cold start sigma from solver_options. 
+    end
+    
+    methods
+        function obj = FullMpcc(model, mpc_options, problem_options, solver_options)
+            obj.mpc_options = mpc_options;
+            obj.problem_options = problem_options;
+            obj.solver_options = solver_options;
+            obj.cold_sigma_0 = solver_options.sigma_0;
+            obj.ocp_solver = nosnoc.ocp.Solver(model, problem_options, solver_options);
+        end
+        
+        function [u, stats] = get_feedback(obj, x0)
+            % This method takes a state estimate $x_0$ and returns the corresponding control $u$.
+            feedback_timer = tic;
+
+            % Update sigma_0 to the fast one if we have 
+            if obj.last_solve_successful
+                obj.solver_options.sigma_0 = obj.mpc_options.fullmpcc_fast_sigma_0;
+            end
+            obj.ocp_solver.set_x0(x0);
+            obj.ocp_solver.solve();
+            u_res = obj.ocp_solver.get('u');
+            u = u_res(:,1);
+            stats.ocp_solver_stats = obj.ocp_solver.stats;
+            stats.feedback_time = toc(feedback_timer);
+        end
+
+        function [stats] = do_preparation(obj)
+            % This method should be called after get feedback to do the work of
+            % setting up the warm starting, it does nothing if the previous solve
+            % did not converge.
+            preparation_timer = tic;
+            if isfield(obj.ocp_solver.stats, "converged") && obj.ocp_solver.stats.converged
+                if obj.mpc_options.fullmpcc_do_shift_initialization
+                    obj.ocp_solver.do_shift_initialization();
+                else
+                    obj.ocp_solver.do_warmstart();
+                end
+                obj.last_solve_successful = true;
+            else
+                obj.last_solve_successful = false;
+            end
+            stats.preparation_time = toc(preparation_timer);
+        end
+
+        function [x] = get_predicted_state(obj)
+            % This method returns the predicted state from the last solve.
+            x_res = obj.ocp_solver.get("x");
+            x = x_res(:,obj.problem_options.N_finite_elements(1)+1);
+        end
+    end
+end
