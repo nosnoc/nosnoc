cc98fe55ec63854063536fb6975424459896e84e
Anton Edvinovich Pozharskiy <apozharski@gmail.com>
2024-10-21 16:17:28 +0200

    add algebraic expressions to Stewart DCS to aid in testablity, improve basic PSS model -> Stewart DCS test

--- test/dcs/TestPssToStewart.m
+++ test/dcs/TestPssToStewart.m
@@ -23,6 +23,13 @@ classdef TestPssToStewart < matlab.unittest.TestCase
             tc.verifyEqual(size(dcs.lambda), [4,1]);
             tc.verifyEqual(dcs.dims.n_mu, 1);
             tc.verifyEqual(size(dcs.mu), [1,1]);
+
+            % Using string comparison for now.
+            % In future it would be nice to be able to check equality
+            % via a walk down the expression graph but that is future work.
+            tc.verifyEqual(str(dcs.f_x), '[(((theta_1_0-theta_1_1)+theta_1_2)-theta_1_3), (((theta_1_0+theta_1_1)-theta_1_2)-theta_1_3)]');
+            tc.verifyEqual(str(dcs.g_ind{1}), '[(-(x_0+x_1)), (x_0-x_1), (x_1-x_0), (x_0+x_1)]');
+            tc.verifyEqual(str(dcs.g_alg), '[(mu_1-((x_0+x_1)+lambda_1_0)), (((x_0-x_1)-lambda_1_1)+mu_1), (((x_1-x_0)-lambda_1_2)+mu_1), (((x_0+x_1)-lambda_1_3)+mu_1), ((((theta_1_0+theta_1_1)+theta_1_2)+theta_1_3)-1)]');
         end
     end
 end
