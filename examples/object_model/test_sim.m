eba90546520cadfc6a4e6e957a35da05822dc5ca
Anton Pozharskiy <apozharski@gmail.com>
2024-11-22 11:33:09 +0100

    add objects support for integrator

--- examples/object_model/test_sim.m
+++ examples/object_model/test_sim.m
@@ -2,12 +2,14 @@ clear all
 close all
 import casadi.*
 
-T = 1.0;
-N_stages = 1;
+T_sim = 5.0;
+N_sim = 5;
 N_finite_elements = 2;
 %% Define (uncontrolled for now) projected system
 model = nosnoc.model.Objects;
-opts = nosnoc.Options;
+problem_options = nosnoc.Options;
+integrator_options = nosnoc.integrator.Options();
+solver_options = integrator_options.fesd_solver_opts;
 
 %A1 = inv(diag([4, 0.1]));
 A1 = inv(diag([1, 1]));
@@ -22,40 +24,28 @@ ellipse2.x_dot = [0;0;0];
 
 model.addContact(ball1, ellipse2, 0.1);
 
-model.r = 0.01;
+problem_options.N_finite_elements = N_finite_elements;
+problem_options.n_s = 2;
+problem_options.cross_comp_mode = CrossCompMode.FE_FE;
 
-opts.T = T;
-opts.N_stages = N_stages;
-opts.N_finite_elements = N_finite_elements;
-opts.n_s = 2;
-opts.cross_comp_mode = CrossCompMode.FE_FE;
+problem_options.N_sim = N_sim;
+problem_options.T_sim = T_sim;
 %opts.step_equilibration = 'mlcp';
 
 
 %% Create problem
+solver_options.homotopy_steering_strategy = "ELL_INF";
+solver_options.decreasing_s_elastic_upper_bound = true;
+solver_options.opts_casadi_nlp.ipopt.max_iter = 5000;
+solver_options.opts_casadi_nlp.ipopt.linear_solver = 'ma27';
+%solver_options.opts_casadi_nlp.ipopt.acceptable_iter = 1;
+%solver_options.opts_casadi_nlp.ipopt.acceptable_tol = 1e-8;
+solver_options.warm_start_duals = true;
+solver_options.complementarity_tol = 1e-9;
+%solver_options.polishing_step = 1;
+%solver_options.N_homotopy = 1;
+solver_options.print_level = 3;
 
-solver_opts = nosnoc.solver.Options();
-%solver_opts.homotopy_steering_strategy = "ELL_INF";
-%solver_opts.decreasing_s_elastic_upper_bound = true;
-solver_opts.opts_casadi_nlp.ipopt.max_iter = 5000;
-solver_opts.opts_casadi_nlp.ipopt.linear_solver = 'ma27';
-%solver_opts.opts_casadi_nlp.ipopt.acceptable_iter = 1;
-%solver_opts.opts_casadi_nlp.ipopt.acceptable_tol = 1e-8;
-solver_opts.warm_start_duals = true;
-solver_opts.complementarity_tol = 1e-9;
-%solver_opts.polishing_step = 1;
-%solver_opts.N_homotopy = 1;
-solver_opts.print_level = 5;
+integrator = nosnoc.Integrator(model, problem_options, integrator_options);
+[t_grid, x_res, t_grid_full, x_res_full] = integrator.simulate();
 
-opts.preprocess();
-model.verify_and_backfill(opts);
-
-dcs = nosnoc.dcs.Objects(model);
-dcs.generate_variables(opts);
-dcs.generate_equations(opts);
-
-dtp = nosnoc.discrete_time_problem.Objects(dcs, opts);
-
-dtp.create_solver(solver_opts);
-
-dtp.solve();
