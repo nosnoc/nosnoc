96ca099e7e68184c17f00845ab275510bbca5a52
Anton Pozharskiy <apozharski@gmail.com>
2024-11-22 13:55:08 +0100

    some more comments

--- +nosnoc/+objects/Object.m
+++ +nosnoc/+objects/Object.m
@@ -3,16 +3,17 @@ classdef Object < handle & matlab.mixin.Heterogeneous
 % - Ball (2 or 3 dimensions)
 % - (union of) Ellipsoid (2 or 3 dimensions)
     properties(GetAccess=public, SetAccess=protected)
-        n_dim
-        name
+        n_dim % Number of dimensions (2 or 3).
+        name % Name used in variable creation.
     end
 
     properties(Constant)
-        num_objects = nosnoc.objects.Counter();
+        num_objects = nosnoc.objects.Counter(); % Class static counter for number of objects of a given type.
     end
 
     methods (Sealed=true)
         function is_eq = eq(a,b)
+        % Check for equality via handle equality.
             is_eq = eq@handle(a,b);
         end
     end
