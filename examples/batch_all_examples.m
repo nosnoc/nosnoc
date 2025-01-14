612eb748dac55d5b10dd8f6d54c81b8a51c26111
Anton Edvinovich Pozharskiy <apozharski@gmail.com>
2025-01-14 10:22:21 +0100

    split run and batch, update example list

--- examples/batch_all_examples.m
+++ examples/batch_all_examples.m
@@ -1,25 +1,24 @@
-clear all;
-set(0,'DefaultFigureVisible','off');
+clear all; close all;
+%set(0,'DefaultFigureVisible','off');
 folder  = '.';
 ftext = readlines("all_examples.txt");
 [path,name,ext] = fileparts(ftext);
 orig_dir = pwd;
-%c = parcluster;
+c = parcluster;
 for ii=1:length(name)
     cd(path(ii));
-    save('ws.mat')
-    try
-        run(name(ii))
-    catch
-        load 'ws.mat'
-        warning([char(path(ii)) '/' char(name(ii)), ' failed'])
-    end
-    clear;
-    close all;
-    %batch(name, 'CaptureDiary', true);
-    %c.Jobs
-    load 'ws.mat'
-    delete 'ws.mat'
+    jobs(ii) = batch(name(ii), 'CaptureDiary', true);
     cd(orig_dir);
 end
 
+while true
+    all_done = true;
+    for job=jobs
+        all_done = all_done & strcmp(job.State, 'finished');
+    end
+    if all_done
+        break
+    end
+    pause(1);
+    jobs.display
+end
