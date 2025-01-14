bfe39f81b9988849f6b17468f297146eabd746ad
Anton Edvinovich Pozharskiy <apozharski@gmail.com>
2025-01-14 14:59:53 +0100

    fixing some more examples

--- examples/batch_all_examples.m
+++ examples/batch_all_examples.m
@@ -5,13 +5,19 @@ ftext = readlines("all_examples.txt");
 [path,name,ext] = fileparts(ftext);
 orig_dir = pwd;
 c = parcluster;
-for ii=1:length(name)
+for ii=43:length(name)
     cd(path(ii));
     jobs(ii) = batch(name(ii), 'CaptureDiary', true);
+
+    msg = char(formattedDisplayText(jobs));
+    update_msg(msg);
+
     cd(orig_dir);
 end
 
 while true
+    msg = char(formattedDisplayText(jobs));
+    update_msg(msg);
     all_done = true;
     for job=jobs
         all_done = all_done & strcmp(job.State, 'finished');
@@ -20,5 +26,15 @@ while true
         break
     end
     pause(1);
-    jobs.display
+end
+
+function update_msg(msg)
+    ASCII_BKSP_CHAR = 8;
+    persistent prev_len;
+    if isempty(prev_len) 
+        prev_len = 0;
+    end
+    
+    disp([ char(repmat(ASCII_BKSP_CHAR,1,prev_len)) msg]);
+    prev_len = numel(msg)+1;
 end
