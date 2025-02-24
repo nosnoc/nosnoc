b47e074049012a1ac5448cb7f7295353457db62f
Anton Edvinovich Pozharskiy <apozharski@gmail.com>
2025-02-24 12:18:25 +0100

    Pss active set for Stewart implemented

---
+++ +nosnoc/+activeset/Base.m
@@ -0,0 +1,5 @@
+classdef Base < handle
+% Base class for all active set objects, currently used just for type verification.
+%
+% TODO(@anton): make this non-instantiable perhaps with a protected constructor
+end
