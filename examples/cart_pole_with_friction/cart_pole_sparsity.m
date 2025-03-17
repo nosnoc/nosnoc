392f4f1fae845ae9dd882f80c0f34db8aeed20dd
Anton Edvinovich Pozharskiy <apozharski@gmail.com>
2025-03-17 11:06:30 +0100

    improve plotting for understanding sparsity pattern

--- examples/cart_pole_with_friction/cart_pole_sparsity.m
+++ examples/cart_pole_with_friction/cart_pole_sparsity.m
@@ -33,6 +33,8 @@
 % This allows to explore the drawbacks of naive smoothing, e.g., if started
 % with a very low smoothing parameter.
 % For more details see: https://www.syscop.de/files/2023ss/nonsmooth_school/ex1_sol.pdf
+close all
+clear all
 %% Model
 F_friction = 2; % Friction force amplitude
 
@@ -44,11 +46,13 @@ x_ref = [0; 180/180*pi; 0; 0]; % target position
 problem_options = nosnoc.Options();
 problem_options.T = 5;  % Time horizon
 problem_options.rk_scheme = RKSchemes.RADAU_IIA;
-problem_options.rk_representation = 'differential';
-problem_options.n_s = 2;
+%problem_options.rk_representation = 'differential';
+n_s = 3;
+problem_options.n_s = n_s;
 problem_options.dcs_mode = 'Stewart';
 problem_options.N_stages = 2; % number of control intervals
-problem_options.N_finite_elements = 2; % number of finite element on every control interval
+N_fe = 2;
+problem_options.N_finite_elements = N_fe; % number of finite element on every control interval
 problem_options.cross_comp_mode = 'FE_FE';
 
 % solver options
@@ -69,4 +73,48 @@ solver_options.sigma_N = 1e-13;
 ocp_solver = nosnoc.ocp.Solver(model, problem_options, solver_options);
 ocp_solver.solve();
 
+close all;
 nlp = ocp_solver.discrete_time_problem.solver.nlp;
+dyn = [];
+alg = [];
+int = [];
+w = [nlp.w.x(0,0,2);nlp.w.lambda(0,0,2)];
+w_alt = [nlp.w.x(0,0,2);nlp.w.lambda(0,0,2)];
+for ii=1:N_fe
+    x = [];
+    lam = [];
+    theta = [];
+    mu = [];
+    for jj=1:n_s
+        dyn = [dyn;nlp.g.dynamics(1,ii,jj)];
+        alg = [alg;nlp.g.algebraic(1,ii,jj)];
+        int = [int;nlp.g.dynamics(1,ii,jj);nlp.g.algebraic(1,ii,jj)];
+        w = [w;nlp.w.x(1,ii,jj);nlp.w.lambda(1,ii,jj);nlp.w.theta(1,ii,jj);nlp.w.mu(1,ii,jj)];
+        x = [x;nlp.w.x(1,ii,jj)];
+        lam = [lam;nlp.w.lambda(1,ii,jj)];
+        theta = [theta;nlp.w.theta(1,ii,jj)];
+        mu = [mu;nlp.w.mu(1,ii,jj)];
+    end
+    w = [w;nlp.w.h(1,ii)]
+    w_alt = [w_alt;x;lam;theta;mu;nlp.w.h(1,ii)];
+end
+w = [w; nlp.w.u(1)];;
+w_alt = [w_alt;nlp.w.u(1)];
+figure
+spy(dyn.jacobian(w))
+title("dyn,interleaved")
+figure
+spy(dyn.jacobian(w_alt))
+title("dyn,blocks")
+figure
+spy(alg.jacobian(w))
+title("alg,interleaved")
+figure
+spy(alg.jacobian(w_alt))
+title("alg,blocks")
+figure
+spy(int.jacobian(w))
+title("int,interleaved")
+figure
+spy(int.jacobian(w_alt))
+title("int,blocks")
